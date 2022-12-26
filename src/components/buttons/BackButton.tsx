398efbfa7ea1acb489cd8e22b365f07095a0df2e
J M Rossy <jm.rossy@gmail.com>
2022-12-26 15:27:13 -0500

    Remove dead code and unused images

--- src/components/buttons/BackButton.tsx
+++
@@ -1,7 +0,0 @@
-import LeftArrow from '../../images/icons/arrow-left-circle.svg';
-
-import { IconButton, IconButtonProps } from './IconButton';
-
-export function BackButton(props: IconButtonProps) {
-  return <IconButton imgSrc={LeftArrow} title="Go back" {...props} />;
-}

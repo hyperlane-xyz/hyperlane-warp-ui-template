6fac6d965cf4b5cefaaafa9d2786343a5ca94a77
J M Rossy <jm.rossy@gmail.com>
2023-03-24 14:40:05 +0200

    Update toWei utils to handle decimals

--- src/consts/values.ts
+++ src/consts/values.ts
@@ -1,2 +1,3 @@
 export const MIN_ROUNDED_VALUE = 0.00001;
 export const DISPLAY_DECIMALS = 5;
+export const STANDARD_TOKEN_DECIMALS = 18;
--- src/utils/amount.ts
+++ src/utils/amount.ts
@@ -1,25 +1,32 @@
 import { formatUnits, parseUnits } from '@ethersproject/units';
 import BigNumber from 'bignumber.js';
 
-import { DISPLAY_DECIMALS, MIN_ROUNDED_VALUE } from '../consts/values';
+import { DISPLAY_DECIMALS, MIN_ROUNDED_VALUE, STANDARD_TOKEN_DECIMALS } from '../consts/values';
 
 import { logger } from './logger';
 
 export type NumberT = BigNumber.Value;
 
-export function fromWei(value: NumberT | null | undefined): number {
+export function fromWei(
+  value: NumberT | null | undefined,
+  decimals = STANDARD_TOKEN_DECIMALS,
+): number {
   if (!value) return 0;
   const valueString = value.toString().trim();
   const flooredValue = new BigNumber(valueString).toFixed(0, BigNumber.ROUND_FLOOR);
-  return parseFloat(formatUnits(flooredValue));
+  return parseFloat(formatUnits(flooredValue, decimals));
 }
 
 // Similar to fromWei above but rounds to set number of decimals
 // with a minimum floor, configured per token
-export function fromWeiRounded(value: NumberT | null | undefined, roundDownIfSmall = true): string {
+export function fromWeiRounded(
+  value: NumberT | null | undefined,
+  decimals = STANDARD_TOKEN_DECIMALS,
+  roundDownIfSmall = true,
+): string {
   if (!value) return '0';
   const flooredValue = new BigNumber(value).toFixed(0, BigNumber.ROUND_FLOOR);
-  const amount = new BigNumber(formatUnits(flooredValue));
+  const amount = new BigNumber(formatUnits(flooredValue, decimals));
   if (amount.isZero()) return '0';
 
   // If amount is less than min value
@@ -31,14 +38,17 @@ export function fromWeiRounded(value: NumberT | null | undefined, roundDownIfSma
   return amount.toFixed(DISPLAY_DECIMALS).toString();
 }
 
-export function toWei(value: NumberT | null | undefined, decimals = 18): BigNumber {
+export function toWei(
+  value: NumberT | null | undefined,
+  decimals = STANDARD_TOKEN_DECIMALS,
+): BigNumber {
   if (!value) return new BigNumber(0);
   const valueString = value.toString().trim();
   const components = valueString.split('.');
   if (components.length === 1) {
     return new BigNumber(parseUnits(valueString, decimals).toString());
   } else if (components.length === 2) {
-    const trimmedFraction = components[1].substring(0);
+    const trimmedFraction = components[1].substring(0, decimals);
     return new BigNumber(parseUnits(`${components[0]}.${trimmedFraction}`, decimals).toString());
   } else {
     throw new Error(`Cannot convert ${valueString} to wei`);

df8c4080437d5ed2e1fa640fd0fc710fb49d5709
J M Rossy <jm.rossy@gmail.com>
2023-03-24 15:30:34 +0200

    Fetch decimals from contract and validate config during route init

--- src/features/tokens/metadata.ts
+++ src/features/tokens/metadata.ts
@@ -1,5 +1,5 @@
 import SyntheticTokenList from '../../consts/tokens.json';
-import { areAddressesEqual, isValidAddress } from '../../utils/addresses';
+import { areAddressesEqual } from '../../utils/addresses';
 
 export function getAllTokens() {
   return SyntheticTokenList.tokens;
@@ -9,10 +9,4 @@ export function getTokenMetadata(chainId: number, tokenAddress: Address) {
   return SyntheticTokenList.tokens.find(
     (t) => t.chainId == chainId && areAddressesEqual(t.address, tokenAddress),
   );
-}
-
-export function getTokenHypCollateralAddress(chainId: number, tokenAddress: Address) {
-  const tokenMetadata = getTokenMetadata(chainId, tokenAddress);
-  const address = tokenMetadata?.hypCollateralAddress || '';
-  return isValidAddress(address) ? address : null;
 }
--- src/features/transfer/TransferTokenCard.tsx
+++ src/features/transfer/TransferTokenCard.tsx
@@ -9,38 +9,41 @@ import { useTokenRoutes } from '../tokens/routes';
 import { TransferTokenForm } from './TransferTokenForm';
 
 export function TransferTokenCard() {
-  const { tokenRoutes, isLoading, hasError } = useTokenRoutes();
+  const { tokenRoutes, isLoading, error: routesError } = useTokenRoutes();
 
   return (
     <Card classes="w-100 sm:w-[31rem] relative">
-      <div className="absolute left-0 right-0 -top-32 xs:-top-24 flex justify-center overflow-hidden z-10">
-        <WideChevron direction="s" height="100%" width="100" rounded={true} />
-      </div>
-      <div className="relative flex items-start justify-between z-20">
-        <h2 className="pl-0.5 text-lg">Send Tokens</h2>
-        {/* <IconButton
+      <>
+        <div className="absolute left-0 right-0 -top-32 xs:-top-24 flex justify-center overflow-hidden z-10">
+          <WideChevron direction="s" height="100%" width="100" rounded={true} />
+        </div>
+        <div className="relative flex items-start justify-between z-20">
+          <h2 className="pl-0.5 text-lg">Send Tokens</h2>
+          {/* <IconButton
           imgSrc={GearIcon}
           width={20}
           height={20}
           title="Settings"
           classes="hover:rotate-90"
         /> */}
-      </div>
-      {tokenRoutes && <TransferTokenForm tokenRoutes={tokenRoutes} />}
-      {isLoading && (
-        <div className="my-24 flex flex-col items-center">
-          <Spinner />
-          <h3 className="mt-5 text-sm text-gray-500">Finding token routes</h3>
         </div>
-      )}
-      {hasError && (
-        <div className="my-32 flex flex-col items-center">
-          <h3 className="text-red-500">Error searching for token routes.</h3>
-          <div className="mt-2 text-sm text-red-500">
-            Please ensure synthetic token list is valid.
+        {tokenRoutes && <TransferTokenForm tokenRoutes={tokenRoutes} />}
+        {isLoading && (
+          <div className="my-24 flex flex-col items-center">
+            <Spinner />
+            <h3 className="mt-5 text-sm text-gray-500">Finding token routes</h3>
           </div>
-        </div>
-      )}
+        )}
+        {routesError && (
+          <div className="my-32 flex flex-col items-center">
+            <h3 className="text-red-500">Error searching for token routes.</h3>
+            <div className="mt-3 text-sm text-red-500">
+              Please ensure synthetic token list is valid.
+            </div>
+            <div className="mt-4 text-xs text-gray-500">{routesError.toString()}</div>
+          </div>
+        )}
+      </>
     </Card>
   );
 }

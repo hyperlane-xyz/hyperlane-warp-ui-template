dac10a068dbd8690fa092e50073eb1f4e149f886
J M Rossy <jm.rossy@gmail.com>
2023-03-27 13:44:15 +0200

    Fix outdated links

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -3,9 +3,8 @@ export const links = {
   explorer: 'https://explorer.hyperlane.xyz',
   discord: 'https://discord.gg/VK9ZUy3aTV',
   github: 'https://github.com/hyperlane-xyz/hyperlane-warp-ui-template',
-  docs: 'https://docs.hyperlane.xyz/hyperlane-docs',
-  chains: 'https://docs.hyperlane.xyz/hyperlane-docs/developers/domains',
-  jobs: 'https://jobs.lever.co/Hyperlane',
+  docs: 'https://docs.hyperlane.xyz',
+  chains: 'https://docs.hyperlane.xyz/docs/resources/domains',
   twitter: 'https://twitter.com/hyperlane_xyz',
   blog: 'https://medium.com/hyperlane',
 };

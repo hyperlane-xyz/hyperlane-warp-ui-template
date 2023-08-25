0235fc025c1dbbd3d8b2b78d72a4cf863469b810
J M Rossy <jm.rossy@gmail.com>
2023-08-25 13:22:48 -0400

    Update zebec token name and logo

--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -7,9 +7,10 @@ export const tokenList: WarpTokenConfig = [
     chainId: 97,
     address: '0x64544969ed7ebf5f083679233325356ebe738930',
     hypCollateralAddress: '0x31b5234A896FbC4b3e2F7237592D054716762131',
-    symbol: 'ZBC',
+    symbol: 'wZBC',
     name: 'Zebec',
     decimals: 18,
+    logoURI: '/logos/zebec.png',
   },
 
   // proteustestnet
@@ -17,9 +18,10 @@ export const tokenList: WarpTokenConfig = [
     type: 'native',
     chainId: 88002,
     hypNativeAddress: '0x34A9af13c5555BAD0783C220911b9ef59CfDBCEf',
-    symbol: 'ZBC',
+    symbol: 'wZBC',
     name: 'Zebec',
     decimals: 18,
+    logoURI: '/logos/zebec.png',
   },
 
   // solanadevnet
@@ -29,8 +31,9 @@ export const tokenList: WarpTokenConfig = [
     address: 'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr',
     hypCollateralAddress: 'PJH5QAbxAqrrnSXfH3GHR8icua8CDFZmo97z91xmpvx',
     name: 'Zebec',
-    symbol: 'ZBC',
+    symbol: 'wZBC',
     decimals: 6,
     isSpl2022: false,
+    logoURI: '/logos/zebec.png',
   },
 ];

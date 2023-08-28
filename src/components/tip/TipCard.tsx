266742f1525030691550ffe61f1dc0ae471a3db5
J M Rossy <jm.rossy@gmail.com>
2023-08-28 11:31:57 -0400

    Remove learn more link

--- src/components/tip/TipCard.tsx
+++ src/components/tip/TipCard.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image';
 import { useState } from 'react';
 
 import { IconButton } from '../../components/buttons/IconButton';
 import { config } from '../../consts/config';
-import { links } from '../../consts/links';
-import InfoCircle from '../../images/icons/info-circle.svg';
 import XCircle from '../../images/icons/x-circle.svg';
 
 export function TipCard() {
@@ -14,19 +11,10 @@ export function TipCard() {
     <div className="relative px-3 py-3 w-100 sm:w-[31rem] bg-blue-500 shadow-lg rounded opacity-95">
       <h2 className="text-white sm:text-lg"> ⚠️ Nautilus Bridge is in deposit-only mode.</h2>
       <div className="flex items-end justify-between">
-        <p className="text-white mt-1.5 text-xs sm:text-sm max-w-[70%]">
+        <p className="text-white mt-1.5 text-xs sm:text-sm">
           Currently, you can bridge from BSC and Solana to Nautilus. Transfers originating Nautilus
           are expected to go live September 1st.
         </p>
-        <a
-          href={links.github}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="ml-2 px-3 py-1.5 flex items-center bg-gray-100 hover:bg-gray-200 active:bg-gray-300 text-xs sm:text-sm text-blue-500 rounded-md transition-all"
-        >
-          <Image src={InfoCircle} width={16} alt="" />
-          <span className="ml-1.5">Learn More</span>
-        </a>
       </div>
       <div className="absolute right-3 top-3 invert">
         <IconButton

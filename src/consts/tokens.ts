d7a4cb8a866981750f51c001075cd7a1be48adb0
Nam Chu Hoai <nambrot@googlemail.com>
2023-09-14 23:29:25 -0400

    Type

--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -1,10 +1,12 @@
+import { TokenType } from '@hyperlane-xyz/hyperlane-token';
+
 export const tokenList = [
   {
     chainId: 11155111,
     name: 'Ether',
     symbol: 'ETH',
     decimals: 18,
-    type: 'native',
+    type: TokenType.native,
     hypNativeAddress: '0x509cF4D8650284618dea0013eE1493a577e434e4',
   },
 ];

531c11303280b888fffd240d6c8c38dbfac81658
J M Rossy <jm.rossy@gmail.com>
2023-10-30 16:21:19 -0400

    Create AutomaticTokenField

--- src/features/tokens/TokenSelectField.tsx
+++ src/features/tokens/TokenSelectField.tsx
@@ -8,7 +8,9 @@ import { isNonFungibleToken } from '../caip/tokens';
 import { TransferFormValues } from '../transfer/types';
 
 import { TokenListModal } from './TokenListModal';
+import { getToken } from './metadata';
 import { RoutesMap } from './routes/types';
+import { getTokenRoutes } from './routes/utils';
 import { TokenMetadata } from './types';
 
 type Props = {
@@ -62,20 +64,7 @@ export function TokenSelectField({
 
   return (
     <>
-      <button
-        type="button"
-        name={field.name}
-        className={`${styles.base} ${disabled ? styles.disabled : styles.enabled}`}
-        onClick={onClick}
-      >
-        <div className="flex items-center">
-          <TokenIcon token={token} size={20} />
-          <span className={`ml-2 ${!token?.symbol && 'text-slate-400'}`}>
-            {token?.symbol || 'Select Token'}
-          </span>
-        </div>
-        <Image src={ChevronIcon} width={12} height={8} alt="" />
-      </button>
+      <TokenButton token={token} name={name} disabled={disabled} onClick={onClick} />
       <TokenListModal
         isOpen={isModalOpen}
         close={() => setIsModalOpen(false)}
@@ -85,6 +74,50 @@ export function TokenSelectField({
         tokenRoutes={tokenRoutes}
       />
     </>
+  );
+}
+
+// Use this instead when the token field should be auto-set based on the chain
+export function AutomaticTokenField({
+  name,
+  originCaip2Id,
+  destinationCaip2Id,
+  tokenRoutes,
+}: Props) {
+  const routes = getTokenRoutes(originCaip2Id, destinationCaip2Id, tokenRoutes);
+  const tokenCaip19Id = routes.length ? routes[0].baseTokenCaip19Id : undefined;
+  const token = tokenCaip19Id ? getToken(tokenCaip19Id) : undefined;
+  return <TokenButton token={token} name={name} disabled={true} />;
+}
+
+function TokenButton({
+  token,
+  name,
+  disabled,
+  onClick,
+  showChevron,
+}: {
+  token?: TokenMetadata;
+  name: string;
+  disabled?: boolean;
+  onClick?: () => void;
+  showChevron?: boolean;
+}) {
+  return (
+    <button
+      type="button"
+      name={name}
+      className={`${styles.base} ${disabled ? styles.disabled : styles.enabled}`}
+      onClick={onClick}
+    >
+      <div className="flex items-center">
+        <TokenIcon token={token} size={20} />
+        <span className={`ml-2 ${!token?.symbol && 'text-slate-400'}`}>
+          {token?.symbol || 'Select Token'}
+        </span>
+      </div>
+      {showChevron && <Image src={ChevronIcon} width={12} height={8} alt="" />}
+    </button>
   );
 }
 

b920459316ffdec1a5cf7070602bc3bc2856a9fb
Nam Chu Hoai <nambrot@googlemail.com>
2023-11-03 23:26:06 -0400

    Add nexus guide

--- src/components/tip/TipCard.tsx
+++ src/components/tip/TipCard.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 
 import { IconButton } from '../../components/buttons/IconButton';
 import { config } from '../../consts/config';
-import { links } from '../../consts/links';
 import InfoCircle from '../../images/icons/info-circle.svg';
 import XCircle from '../../images/icons/x-circle.svg';
 import { Card } from '../layout/Card';
@@ -13,20 +12,19 @@ export function TipCard() {
   if (!show) return null;
   return (
     <Card className="w-100 sm:w-[31rem]">
-      <h2 className="text-blue-500 sm:text-lg">Bridge Tokens with Hyperlane Warp Routes!</h2>
+      <h2 className="text-blue-500 sm:text-lg">Bridge Celestia's TIA with Nexus!</h2>
       <div className="flex items-end justify-between">
         <p className="mt-1 text-xs sm:text-sm max-w-[70%]">
-          Warp Routes make it easy to permissionlessly take your tokens interchain. Fork this
-          template to get started!
+          Nexus is the interface for navigating the modular world.
         </p>
         <a
-          href={links.github}
+          href="https://web3godot.notion.site/Bridging-TIA-to-Manta-Pacific-0d8edccead804255a6f06d838b80d13d"
           target="_blank"
           rel="noopener noreferrer"
           className="ml-2 px-3 py-1.5 flex items-center bg-gray-100 hover:bg-gray-200 active:bg-gray-300 text-xs sm:text-sm text-blue-500 rounded-full transition-all"
         >
           <Image src={InfoCircle} width={16} alt="" />
-          <span className="ml-1.5">Learn More</span>
+          <span className="ml-1.5">Guide</span>
         </a>
       </div>
       <div className="absolute right-3 top-3">

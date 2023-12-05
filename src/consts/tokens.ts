6de1cedf6b267b6cb7860f87acfd9f18c0cc03f0
J M Rossy <jm.rossy@gmail.com>
2023-12-05 10:50:22 -0500

    Update comment

--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -1,6 +1,6 @@
 import { WarpTokenConfig } from '../features/tokens/types';
 
-// A list of warp token configs
+// A list of Warp UI token configs
 // Tokens can be defined here, in tokens.json, or in tokens.yaml
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const tokenList: WarpTokenConfig = [

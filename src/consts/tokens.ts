2623034901daee592eed03bb60617b60f65ff9b9
J M Rossy <jm.rossy@gmail.com>
2023-12-26 10:17:39 -0500

    Enable viction token routes

--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -75,38 +75,38 @@ export const tokenList: WarpTokenConfig = [
     logoURI: '/logos/ECLIP.png',
   },
 
-  // // Ethereum Eth to Viction
-  // {
-  //   type: 'native',
-  //   chainId: 1,
-  //   name: 'Ether',
-  //   symbol: 'ETH',
-  //   decimals: 18,
-  //   hypNativeAddress: '0x15b5D6B614242B118AA404528A7f3E2Ad241e4A4',
-  //   logoURI: '/logos/weth.png',
-  // },
+  // Ethereum Eth to Viction
+  {
+    type: 'native',
+    chainId: 1,
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18,
+    hypNativeAddress: '0x15b5D6B614242B118AA404528A7f3E2Ad241e4A4',
+    logoURI: '/logos/weth.png',
+  },
 
-  // // Ethereum USDC to Viction
-  // {
-  //   type: 'collateral',
-  //   chainId: 1,
-  //   name: 'USDC',
-  //   symbol: 'USDC',
-  //   decimals: 18,
-  //   address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-  //   hypCollateralAddress: '0x31Dca7762930f56D81292f85E65c9D67575804fE',
-  //   logoURI: '/logos/usdc.svg',
-  // },
+  // Ethereum USDC to Viction
+  {
+    type: 'collateral',
+    chainId: 1,
+    name: 'USDC',
+    symbol: 'USDC',
+    decimals: 18,
+    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    hypCollateralAddress: '0x31Dca7762930f56D81292f85E65c9D67575804fE',
+    logoURI: '/logos/usdc.svg',
+  },
 
-  // // Ethereum USDT to Viction
-  // {
-  //   type: 'collateral',
-  //   chainId: 1,
-  //   name: 'USDT',
-  //   symbol: 'USDT',
-  //   decimals: 18,
-  //   address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
-  //   hypCollateralAddress: '0x4221a16A01F61c2b38A03C52d828a7041f6AAA49',
-  //   logoURI: '/logos/usdt.svg',
-  // },
+  // Ethereum USDT to Viction
+  {
+    type: 'collateral',
+    chainId: 1,
+    name: 'USDT',
+    symbol: 'USDT',
+    decimals: 18,
+    address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    hypCollateralAddress: '0x4221a16A01F61c2b38A03C52d828a7041f6AAA49',
+    logoURI: '/logos/usdt.svg',
+  },
 ];

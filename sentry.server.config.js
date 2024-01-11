e716220d9784f7a76a5ba16cd2b01bec7018cf55
J M Rossy <jm.rossy@gmail.com>
2024-01-11 18:15:31 -0500

    Strip down to minimal integrations

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,8 +1,6 @@
+import { sentryDefaultConfig } from './sentry.default.config';
 import * as Sentry from '@sentry/nextjs';
 
-if (process.env.SENTRY_DSN) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    tracesSampleRate: 1.0,
-  });
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  Sentry.init(sentryDefaultConfig)
 }

ee9db42731487b146510b746b72f2713ab8d69ed
J M Rossy <jm.rossy@gmail.com>
2024-01-11 16:29:08 -0500

    Add error captures

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,23 @@
 /* eslint-disable no-console */
+import { captureException } from '@sentry/nextjs';
+
+import { getAddressProtocolType } from '@hyperlane-xyz/utils';
 
 export const logger = {
   debug: (...args: any[]) => console.debug(...args),
   info: (...args: any[]) => console.info(...args),
   warn: (...args: any[]) => console.warn(...args),
-  error: (...args: any[]) => console.error(...args),
+  error: (message: string, error: any, ...args: any[]) => {
+    console.error(...args);
+    const filteredArgs = args.filter(isSafeSentryArg);
+    const extra = filteredArgs.reduce((acc, arg, i) => ({ ...acc, [`arg${i}`]: arg }), {});
+    extra['message'] = message;
+    captureException(error, { extra });
+  },
 };
+
+function isSafeSentryArg(arg: any) {
+  if (typeof arg == 'number') return true;
+  if (typeof arg == 'string') return !getAddressProtocolType(arg) && arg.length < 1000;
+  return false;
+}

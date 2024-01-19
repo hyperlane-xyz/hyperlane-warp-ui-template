26a05b01083df47e9feff6e2d1b5bbb524375ad1
J M Rossy <jm.rossy@gmail.com>
2024-01-18 19:46:52 -0500

    Add placeholder json files

--- src/utils/runtime.ts
+++
@@ -1,7 +0,0 @@
-export function isBrowser() {
-  return typeof window !== 'undefined' && typeof window.document !== 'undefined';
-}
-
-export function isNode() {
-  return typeof process !== 'undefined';
-}

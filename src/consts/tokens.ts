101a4aef808f5e3577e8ec784edd01714ad17085
J M Rossy <jm.rossy@gmail.com>
2024-01-22 16:41:10 -0500

    Update cosmjs injective patch to avoid sdk dep

--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -5,26 +5,36 @@ import { WarpTokenConfig } from '../features/tokens/types';
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const tokenList: WarpTokenConfig = [
   // Example collateral token for an EVM chain
-  {
-    type: 'collateral',
-    chainId: 5,
-    address: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
-    hypCollateralAddress: '0x145de8760021c4ac6676376691b78038d3DE9097',
-    name: 'Weth',
-    symbol: 'WETH',
-    decimals: 18,
-    logoURI: '/logos/weth.png', // See public/logos/
-  },
+  // {
+  //   type: 'collateral',
+  //   chainId: 5,
+  //   address: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+  //   hypCollateralAddress: '0x145de8760021c4ac6676376691b78038d3DE9097',
+  //   name: 'Weth',
+  //   symbol: 'WETH',
+  //   decimals: 18,
+  //   logoURI: '/logos/weth.png', // See public/logos/
+  // },
 
   // Example NFT (ERC721) token for an EVM chain
+  // {
+  //   chainId: 5,
+  //   name: 'Test721',
+  //   symbol: 'TEST721',
+  //   decimals: 0,
+  //   type: 'collateral',
+  //   address: '0x77566D540d1E207dFf8DA205ed78750F9a1e7c55',
+  //   hypCollateralAddress: '0xDcbc0faAA269Cf649AC8950838664BB7B355BD6B',
+  //   isNft: true,
+  // },
+
   {
-    chainId: 5,
-    name: 'Test721',
-    symbol: 'TEST721',
-    decimals: 0,
-    type: 'collateral',
-    address: '0x77566D540d1E207dFf8DA205ed78750F9a1e7c55',
-    hypCollateralAddress: '0xDcbc0faAA269Cf649AC8950838664BB7B355BD6B',
-    isNft: true,
+    type: 'native',
+    chainId: 'injective-888',
+    name: 'Injective Coin',
+    symbol: 'INJ',
+    decimals: 6,
+    hypNativeAddress: 'inj1wn32e0e07yytd4m03n8mynnp6qzgg75qmhsq88',
+    logoURI: '/logos/injective.svg',
   },
 ];

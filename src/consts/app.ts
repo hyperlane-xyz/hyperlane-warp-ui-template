28f7008b3369b24e7cdc8795c987658c5bc89c1f
J M Rossy <jm.rossy@gmail.com>
2024-01-26 14:56:36 -0500

    Set correct app url

--- src/consts/app.ts
+++ src/consts/app.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = 'Injective Token Bridge';
 export const APP_DESCRIPTION = 'A token bridge for Injective powered by Hyperlane';
-export const APP_URL = 'https://injective.com';
+export const APP_URL = 'https://inevmbridge.com';

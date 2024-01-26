5d0446e11ce7f8cc189aa62cd0bdafabe0842a3f
J M Rossy <jm.rossy@gmail.com>
2024-01-26 15:44:08 -0500

    Decrease header image size

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -9,7 +9,7 @@ export function Header() {
     <header className="px-2 sm:px-6 lg:px-12 pt-3 pb-2 w-full">
       <div className="flex items-start justify-between">
         <Link href="/" className="py-2 flex items-center">
-          <Image src={Name} width={180} alt="" className="mt-0.5" />
+          <Image src={Name} width={170} alt="" className="mt-0.5" />
         </Link>
         <div className="flex flex-col items-end md:flex-row-reverse md:items-start gap-2">
           <WalletControlBar />

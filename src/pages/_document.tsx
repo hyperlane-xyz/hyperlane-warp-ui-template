408ca29d4e26f4f9ba736758813c208ee1850f20
J M Rossy <jm.rossy@gmail.com>
2024-02-01 11:32:44 -0500

    Replace favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#31D99C" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <meta name="msapplication-TileColor" content="#31D99C" />
         <meta name="theme-color" content="#ffffff" />
 

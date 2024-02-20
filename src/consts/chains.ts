69804808b63a59c56a9b51853f3f266604252f8a
J M Rossy <jm.rossy@gmail.com>
2024-02-20 12:42:24 -0500

    configure plume testnet route

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -1,4 +1,5 @@
-import { ChainMap, ChainMetadata } from '@hyperlane-xyz/sdk';
+import { ChainMap, ChainMetadata, ExplorerFamily } from '@hyperlane-xyz/sdk';
+import { ProtocolType } from '@hyperlane-xyz/utils';
 
 // A map of chain names to ChainMetadata
 // Chains can be defined here, in chains.json, or in chains.yaml
@@ -28,4 +29,31 @@ export const chains: ChainMap<ChainMetadata & { mailbox?: Address }> = {
   //   },
   //   logoURI: '/logo.svg',
   // },
+  plumetestnet: {
+    blockExplorers: [
+      {
+        apiUrl: 'https://plume-testnet.explorer.caldera.xyz/api',
+        family: ExplorerFamily.Blockscout,
+        name: 'Plume Testnet Explorer',
+        url: 'https://plume-testnet.explorer.caldera.xyz',
+      },
+    ],
+    blocks: {
+      confirmations: 1,
+      estimateBlockTime: 3,
+      reorgPeriod: 1,
+    },
+    chainId: 161221135,
+    displayName: 'Plume Testnet',
+    domainId: 161221135,
+    isTestnet: true,
+    name: 'plumetestnet',
+    nativeToken: {
+      decimals: 18,
+      name: 'Ether',
+      symbol: 'ETH',
+    },
+    protocol: ProtocolType.Ethereum,
+    rpcUrls: [{ http: 'https://plume-testnet.rpc.caldera.xyz/http' }],
+  },
 };
--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -5,26 +5,37 @@ import { WarpTokenConfig } from '../features/tokens/types';
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const tokenList: WarpTokenConfig = [
   // Example collateral token for an EVM chain
-  {
-    type: 'collateral',
-    chainId: 5,
-    address: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
-    hypCollateralAddress: '0x145de8760021c4ac6676376691b78038d3DE9097',
-    name: 'Weth',
-    symbol: 'WETH',
-    decimals: 18,
-    logoURI: '/logos/weth.png', // See public/logos/
-  },
+  // {
+  //   type: 'collateral',
+  //   chainId: 5,
+  //   address: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+  //   hypCollateralAddress: '0x145de8760021c4ac6676376691b78038d3DE9097',
+  //   name: 'Weth',
+  //   symbol: 'WETH',
+  //   decimals: 18,
+  //   logoURI: '/logos/weth.png', // See public/logos/
+  // },
 
   // Example NFT (ERC721) token for an EVM chain
+  // {
+  //   chainId: 5,
+  //   name: 'Test721',
+  //   symbol: 'TEST721',
+  //   decimals: 0,
+  //   type: 'collateral',
+  //   address: '0x77566D540d1E207dFf8DA205ed78750F9a1e7c55',
+  //   hypCollateralAddress: '0xDcbc0faAA269Cf649AC8950838664BB7B355BD6B',
+  //   isNft: true,
+  // },
+
+  // Sepolia Eth to Plume
   {
-    chainId: 5,
-    name: 'Test721',
-    symbol: 'TEST721',
-    decimals: 0,
-    type: 'collateral',
-    address: '0x77566D540d1E207dFf8DA205ed78750F9a1e7c55',
-    hypCollateralAddress: '0xDcbc0faAA269Cf649AC8950838664BB7B355BD6B',
-    isNft: true,
+    type: 'native',
+    chainId: 11155111,
+    hypNativeAddress: '0x8C159cc81F396E474A0b201f770c9944885A537A',
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18,
+    logoURI: '/logos/weth.png',
   },
 ];

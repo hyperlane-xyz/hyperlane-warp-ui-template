f8a311df46db6d9d2b17c756b189b0b859a75281
J M Rossy <jm.rossy@gmail.com>
2024-03-07 11:34:55 -0500

    Add sentry filter list

--- sentry.default.config.js
+++ sentry.default.config.js
@@ -1,6 +1,6 @@
 export const sentryDefaultConfig = {
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 1,
   maxBreadcrumbs: 1,
   sendClientReports: false,
   attachStacktrace: false,
@@ -10,4 +10,26 @@ export const sentryDefaultConfig = {
     delete event.user;
     return event;
   },
+  // Based on example from https://docs.sentry.io/platforms/javascript/configuration/filtering
+  ignoreErrors: [
+    // Hyperlane custom set
+    "'defineProperty' on proxy", // Error from wallet lib
+    "Refused to create a WebAssembly object", // CSP blocking wasm
+    "call to WebAssembly.instantiate() blocked by CSP", // Same ^
+    "Request rejected", // Unknown noise during Next.js init
+    "WebSocket connection failed for host: wss://relay.walletconnect.org", // WalletConnect flakiness
+    "Socket stalled when trying to connect to wss://relay.walletconnect.org", // Same ^
+    // Some recommendations from https://docs.sentry.io/platforms/javascript/configuration/filtering
+    "top.GLOBALS",
+    "originalCreateNotification",
+    "canvas.contentDocument",
+    "MyApp_RemoveAllHighlights",
+    "atomicFindClose",
+  ],
+  denyUrls: [
+    // Chrome extensions
+    /extensions\//i,
+    /^chrome:\/\//i,
+    /^chrome-extension:\/\//i,
+  ],
 };

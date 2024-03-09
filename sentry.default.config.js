d01d616e7791bd8d467101f01b41c6519172777b
J M Rossy <jm.rossy@gmail.com>
2024-03-09 14:26:55 -0500

    Tweak sentry filters

--- sentry.default.config.js
+++ sentry.default.config.js
@@ -1,31 +1,45 @@
+const filters = [
+  // Hyperlane custom set
+  "trap returned falsish for property", // Error from wallet lib
+  "Refused to create a WebAssembly object", // CSP blocking wasm
+  "call to WebAssembly.instantiate", // Same ^
+  "Request rejected", // Unknown noise during Next.js init
+  "WebSocket connection failed for host", // WalletConnect flakiness
+  "Socket stalled when trying to connect", // Same ^
+  // Some recommendations from https://docs.sentry.io/platforms/javascript/configuration/filtering
+  "top.GLOBALS",
+  "originalCreateNotification",
+  "canvas.contentDocument",
+  "MyApp_RemoveAllHighlights",
+  "atomicFindClose",
+]
+
 export const sentryDefaultConfig = {
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.5,
   maxBreadcrumbs: 1,
   sendClientReports: false,
   attachStacktrace: false,
   defaultIntegrations: false,
   integrations: [],
-  beforeSend(event) {
+  beforeSend(event, hint) {
+    if (event && event.message && 
+      filters.find((f) => event.message.match(f))) 
+    {
+      return null;
+    }
+
+    const error = hint.originalException;
+    if (error && error.message && 
+      filters.find((f) => error.message.match(f))) 
+    {
+      return null;
+    } 
+
     delete event.user;
     return event;
   },
-  // Based on example from https://docs.sentry.io/platforms/javascript/configuration/filtering
-  ignoreErrors: [
-    // Hyperlane custom set
-    "'defineProperty' on proxy", // Error from wallet lib
-    "Refused to create a WebAssembly object", // CSP blocking wasm
-    "call to WebAssembly.instantiate() blocked by CSP", // Same ^
-    "Request rejected", // Unknown noise during Next.js init
-    "WebSocket connection failed for host: wss://relay.walletconnect.org", // WalletConnect flakiness
-    "Socket stalled when trying to connect to wss://relay.walletconnect.org", // Same ^
-    // Some recommendations from https://docs.sentry.io/platforms/javascript/configuration/filtering
-    "top.GLOBALS",
-    "originalCreateNotification",
-    "canvas.contentDocument",
-    "MyApp_RemoveAllHighlights",
-    "atomicFindClose",
-  ],
+  ignoreErrors: filters,
   denyUrls: [
     // Chrome extensions
     /extensions\//i,

3ec5c7a8ea439d50e3f2a3846b5518d44e812554
Trevor Porter <trkporter@ucdavis.edu>
2024-04-12 12:06:07 -0400

    Add usdc

--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -317,6 +317,39 @@ export const tokenConfigs: WarpCoreConfig = {
     },
 
     //#endregion viction
+    //#region ancient8
+
+    {
+      name: 'USD Coin',
+      symbol: 'USDC',
+      decimals: 6,
+      chainName: 'ethereum',
+      standard: TokenStandard.EvmHypCollateral,
+      addressOrDenom: '0x8b4192B9Ad1fCa440A5808641261e5289e6de95D',
+      collateralAddressOrDenom: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+      connections: [
+        {
+          token: 'ethereum|ancient8|0x97423A68BAe94b5De52d767a17aBCc54c157c0E5',
+        },
+      ],
+      logoURI: '/logos/usdc.svg',
+    },
+    {
+      name: 'USD Coin',
+      symbol: 'USDC',
+      decimals: 6,
+      chainName: 'ancient8',
+      standard: TokenStandard.EvmHypSynthetic,
+      addressOrDenom: '0x97423A68BAe94b5De52d767a17aBCc54c157c0E5',
+      connections: [
+        {
+          token: 'ethereum|ethereum|0x8b4192B9Ad1fCa440A5808641261e5289e6de95D',
+        },
+      ],
+      logoURI: '/logos/usdc.svg',
+    },
+
+    //#endregion ancient8
   ],
   options: {
     interchainFeeConstants: [

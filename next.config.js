ac1927e11bb92bc7d8670c56ee1abd5346396f59
zkfarmoor <166660193+zkfarmoor@users.noreply.github.com>
2024-05-14 17:15:54 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const { version } = require('./package.json')
-const { withSentryConfig } = require("@sentry/nextjs");
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })

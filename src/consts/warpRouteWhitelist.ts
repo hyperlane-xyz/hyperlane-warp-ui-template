033f9b31f50bf5eda4a0d02233272fa16f4553bd
J M Rossy <jm.rossy@gmail.com>
2024-06-08 08:53:50 +0800

    Fix comment in warp route whitelist example

--- src/consts/warpRouteWhitelist.ts
+++ src/consts/warpRouteWhitelist.ts
@@ -1,6 +1,7 @@
 // A list of warp route config IDs to be included in the app
+// Warp Route IDs use format `SYMBOL/chainname1-chainname2...` where chains are ordered alphabetically
 // If left empty, all warp routes in the configured registry will be included
 export const warpRouteWhitelist: Array<string> | undefined = [
   // Example:
-  // 'ETH|ethereum-viction'
+  // 'ETH/ethereum-viction'
 ];

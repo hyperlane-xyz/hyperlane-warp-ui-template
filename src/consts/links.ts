6dfa20630647a022294d915cb40a1d13bd7c27e5
Avi Atkin <103125634+avious00@users.noreply.github.com>
2024-06-18 15:15:43 -0400

    Update links.ts

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,11 +1,11 @@
 export const links = {
   home: 'https://www.hyperlane.xyz',
   explorer: 'https://explorer.hyperlane.xyz',
-  discord: 'https://discord.gg/VK9ZUy3aTV',
+  discord: 'https://discord.gg/hyperlane',
   github: 'https://github.com/hyperlane-xyz/hyperlane-warp-ui-template',
   docs: 'https://docs.hyperlane.xyz',
   gasDocs: 'https://docs.hyperlane.xyz/docs/reference/hooks/interchain-gas',
   chains: 'https://docs.hyperlane.xyz/docs/resources/domains',
-  twitter: 'https://twitter.com/hyperlane',
+  twitter: 'https://x.com/hyperlane',
   blog: 'https://medium.com/hyperlane',
 };

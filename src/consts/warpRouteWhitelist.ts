fdd2d00b02bbe099bef55e7508ee871539efc19f
J M Rossy <jm.rossy@gmail.com>
2024-08-04 17:01:59 -0400

    Run prettier

--- src/consts/warpRouteWhitelist.ts
+++ src/consts/warpRouteWhitelist.ts
@@ -3,25 +3,25 @@
 // If left null, all warp routes in the configured registry will be included
 // If set to a list (including an empty list), only the specified routes will be included
 export const warpRouteWhitelist: Array<string> | null = [
-    // TIA routes
-    'TIA.n/arbitrum-neutron',
-    'TIA.n/mantapacific-neutron',
-  
-    // ETH routes
-    'ETH/ethereum-viction',
-  
-    // ECLIP routes
-    'ECLIP/arbitrum-neutron',
-  
-    // USDC routes
-    'USDC/ethereum-inevm',
-    'USDC/ancient8-ethereum',
-    'USDC/ethereum-viction',
-  
-    // USDT routes
-    'USDT/ethereum-inevm',
-    'USDT/ethereum-viction',
-  
-    // INJ routes
-    'INJ/inevm-injective',
-]
+  // TIA routes
+  'TIA.n/arbitrum-neutron',
+  'TIA.n/mantapacific-neutron',
+
+  // ETH routes
+  'ETH/ethereum-viction',
+
+  // ECLIP routes
+  'ECLIP/arbitrum-neutron',
+
+  // USDC routes
+  'USDC/ethereum-inevm',
+  'USDC/ancient8-ethereum',
+  'USDC/ethereum-viction',
+
+  // USDT routes
+  'USDT/ethereum-inevm',
+  'USDT/ethereum-viction',
+
+  // INJ routes
+  'INJ/inevm-injective',
+];

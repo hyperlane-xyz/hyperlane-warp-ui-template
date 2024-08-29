e0f2b645323e3c67985115d070f67d070a16af20
Connor McEwen <connor.mcewen@gmail.com>
2024-08-29 15:02:35 -0700

    add warning

--- src/components/tip/TipCard.tsx
+++ src/components/tip/TipCard.tsx
@@ -1,27 +1,11 @@
-import { useState } from 'react';
-
-import { IconButton } from '../../components/buttons/IconButton';
-import { config } from '../../consts/config';
-import XCircle from '../../images/icons/x-circle.svg';
-
 export function TipCard() {
-  const [show, setShow] = useState(config.showTipBox);
-  if (!show) return null;
   return (
-    <div className="relative px-3 py-3 w-100 sm:w-[31rem] bg-blue-500 shadow-lg rounded opacity-95">
-      <h2 className="text-white sm:text-lg"> ⚠️ Nautilus Bridge is in deposit-only mode.</h2>
+    <div className="relative px-3 py-3 w-100 sm:w-[31rem] bg-red-500 shadow-lg rounded opacity-95">
+      <h2 className="text-white sm:text-lg"> ⚠️ Nautilus Bridge is shutting down September 6</h2>
       <div className="flex items-end justify-between">
         <p className="text-white mt-1.5 text-xs sm:text-sm">
-          Currently, bridge transfers to Nautilus must originate from BSC or Solana.
+          You must move funds from Nautilus Chain to BSC or Solana or funds will be lost.
         </p>
-      </div>
-      <div className="absolute right-3 top-3 invert">
-        <IconButton
-          imgSrc={XCircle}
-          onClick={() => setShow(false)}
-          title="Hide tip"
-          classes="hover:rotate-90"
-        />
       </div>
     </div>
   );

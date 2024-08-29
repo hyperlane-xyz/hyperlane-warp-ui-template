7f3e83c24b682fb69a43f69d95cb3054b736739e
Connor McEwen <connor.mcewen@gmail.com>
2024-08-29 15:08:20 -0700

    clarify language

--- src/components/tip/TipCard.tsx
+++ src/components/tip/TipCard.tsx
@@ -1,7 +1,7 @@
 export function TipCard() {
   return (
     <div className="relative px-3 py-3 w-100 sm:w-[31rem] bg-red-500 shadow-lg rounded opacity-95">
-      <h2 className="text-white sm:text-lg"> ⚠️ Nautilus Bridge is shutting down September 6</h2>
+      <h2 className="text-white sm:text-lg"> ⚠️ Nautilus Chain is shutting down September 6</h2>
       <div className="flex items-end justify-between">
         <p className="text-white mt-1.5 text-xs sm:text-sm">
           You must move funds from Nautilus Chain to BSC or Solana or funds will be lost.

295dc2a94985ff085614465ee66fc354273c55f9
J M Rossy <jm.rossy@gmail.com>
2024-09-18 17:04:49 +0100

    Create new WarningBanner component (#242)
    - Create new WarningBanner component
    - Add chainWalletWhitelists config
    - Add warning in form when chains require certain wallets

--- src/features/transfer/TransferTokenForm.tsx
+++ src/features/transfer/TransferTokenForm.tsx
@@ -18,6 +18,7 @@ import SwapIcon from '../../images/icons/swap.svg';
 import { Color } from '../../styles/Color';
 import { logger } from '../../utils/logger';
 import { ChainSelectField } from '../chains/ChainSelectField';
+import { ChainWalletWarning } from '../chains/ChainWalletWarning';
 import { getChainDisplayName } from '../chains/utils';
 import { useIsAccountSanctioned } from '../sanctions/hooks/useIsAccountSanctioned';
 import { useStore } from '../store';
@@ -62,8 +63,9 @@ export function TransferTokenForm() {
       validateOnChange={false}
       validateOnBlur={false}
     >
-      {({ isValidating }) => (
-        <Form className="flex flex-col items-stretch w-full mt-2">
+      {({ isValidating, values }) => (
+        <Form className="flex flex-col items-stretch w-full">
+          <ChainWalletWarning originChain={values.origin} />
           <ChainSelectSection isReview={isReview} />
           <div className="mt-3 flex justify-between items-end space-x-4">
             <TokenSection setIsNft={setIsNft} isReview={isReview} />
@@ -112,7 +114,7 @@ function ChainSelectSection({ isReview }: { isReview: boolean }) {
   const chains = useMemo(() => getWarpCore().getTokenChains(), []);
 
   return (
-    <div className="flex items-center justify-center space-x-7 sm:space-x-10">
+    <div className="mt-2 flex items-center justify-center space-x-7 sm:space-x-10">
       <ChainSelectField name="origin" label="From" chains={chains} disabled={isReview} />
       <div className="flex flex-col items-center">
         <div className="flex mb-6 sm:space-x-1.5">

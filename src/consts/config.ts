ca9c9afee83cb21ed07fa2b21c6dda1a9d8fab21
J M Rossy <jm.rossy@gmail.com>
2024-09-25 17:05:06 -0400

    Update hyp packages to 5.3.0 and organize configs (#266)
    * Improve error message for confirmation timeouts
    
    * Organize config values alphabetically
    
    * Update hyp packages to 5.3.0

--- src/consts/config.ts
+++ src/consts/config.ts
@@ -12,31 +12,31 @@ const transferBlacklist = process?.env?.NEXT_PUBLIC_TRANSFER_BLACKLIST || '';
 const chainWalletWhitelists = JSON.parse(process?.env?.NEXT_PUBLIC_CHAIN_WALLET_WHITELISTS || '{}');
 
 interface Config {
-  isDevMode: boolean; // Enables some debug features in the app
-  version: string; // Matches version number in package.json
-  registryUrl: string | undefined; // Optional URL to use a custom registry instead of the published canonical version
-  explorerApiKeys: Record<string, string>; // Optional map of API keys for block explorer
-  showTipBox: boolean; // Show/Hide the blue tip box above the transfer form
-  showDisabledTokens: boolean; // Show/Hide invalid token options in the selection modal
-  walletConnectProjectId: string; // Project ID provided by walletconnect
-  withdrawalWhitelist: string; // comma-separated list of CAIP2 chain IDs to which transfers are supported
-  transferBlacklist: string; // comma-separated list of routes between which transfers are disabled. Expects Caip2Id-Caip2Id (e.g. ethereum:1-sealevel:1399811149)
-  enableExplorerLink: boolean; // Include a link to the hyperlane explorer in the transfer modal
   addressBlacklist: string[]; // A list of addresses that are blacklisted and cannot be used in the app
   chainWalletWhitelists: ChainMap<string[]>; // A map of chain names to a list of wallet names that work for it
+  enableExplorerLink: boolean; // Include a link to the hyperlane explorer in the transfer modal
+  explorerApiKeys: Record<string, string>; // Optional map of API keys for block explorer
+  isDevMode: boolean; // Enables some debug features in the app
+  registryUrl: string | undefined; // Optional URL to use a custom registry instead of the published canonical version
+  showDisabledTokens: boolean; // Show/Hide invalid token options in the selection modal
+  showTipBox: boolean; // Show/Hide the blue tip box above the transfer form
+  transferBlacklist: string; // comma-separated list of routes between which transfers are disabled. Expects Caip2Id-Caip2Id (e.g. ethereum:1-sealevel:1399811149)
+  version: string; // Matches version number in package.json
+  walletConnectProjectId: string; // Project ID provided by walletconnect
+  withdrawalWhitelist: string; // comma-separated list of CAIP2 chain IDs to which transfers are supported
 }
 
 export const config: Config = Object.freeze({
-  isDevMode,
-  version,
-  registryUrl,
-  explorerApiKeys,
-  showTipBox: true,
-  showDisabledTokens: true,
-  walletConnectProjectId,
-  withdrawalWhitelist,
-  transferBlacklist,
-  enableExplorerLink: false,
   addressBlacklist: ADDRESS_BLACKLIST.map((address) => address.toLowerCase()),
   chainWalletWhitelists,
+  enableExplorerLink: false,
+  explorerApiKeys,
+  isDevMode,
+  registryUrl,
+  showDisabledTokens: true,
+  showTipBox: true,
+  version,
+  transferBlacklist,
+  walletConnectProjectId,
+  withdrawalWhitelist,
 });

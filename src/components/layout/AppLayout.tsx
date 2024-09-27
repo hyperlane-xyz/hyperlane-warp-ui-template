6c54d837fac4101201c581b123f412182de4d639
J M Rossy <jm.rossy@gmail.com>
2024-09-27 12:18:20 -0400

    Move font class up to body tag

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { PropsWithChildren } from 'react';
 
-import { APP_NAME, BACKGROUND_COLOR, BACKGROUND_IMAGE, MAIN_FONT } from '../../consts/app';
+import { APP_NAME, BACKGROUND_COLOR, BACKGROUND_IMAGE } from '../../consts/app';
 import { Footer } from '../nav/Footer';
 import { Header } from '../nav/Header';
 
@@ -16,7 +16,7 @@ export function AppLayout({ children }: PropsWithChildren) {
       <div
         style={styles.container}
         id="app-content"
-        className={`relative flex flex-col justify-between h-full min-h-screen w-full min-w-screen ${MAIN_FONT.className}`}
+        className="relative flex flex-col justify-between h-full min-h-screen w-full min-w-screen"
       >
         <Header />
         <div className="sm:px-4 mx-auto grow flex items-center max-w-screen-xl">

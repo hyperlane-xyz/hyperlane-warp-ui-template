9fcb78c4a014baffa961618e029749a51715a4d5
Xaroz <jasonguo2013jg@gmail.com>
2024-10-16 14:55:07 -0400

    chore: organize imports

--- src/components/nav/Footer.tsx
+++ src/components/nav/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-
 import React from 'react';
+
 import { links } from '../../consts/links';
 import { Color } from '../../styles/Color';
 import { Discord } from '../icons/Discord';

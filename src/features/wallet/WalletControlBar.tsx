7d64f2bb5607a1fa049597a753d5e096573ff5aa
Jason Guo <33064781+Xaroz@users.noreply.github.com>
2024-10-23 11:27:10 -0400

    Feat: Transaction history improvements (#298)
    * feat: add chevron icon for clarity
    
    * feat: wallet floating buttons
    
    * chore: added warp docs link and sort imports
    
    ---------
    
    Co-authored-by: J M Rossy <jm.rossy@gmail.com>

--- src/features/wallet/WalletControlBar.tsx
+++ src/features/wallet/WalletControlBar.tsx
@@ -1,22 +1,23 @@
 import Image from 'next/image';
-import { useState } from 'react';
 
 import { ProtocolType, shortenAddress } from '@hyperlane-xyz/utils';
+import { ChevronIcon } from '@hyperlane-xyz/widgets';
 
 import { SolidButton } from '../../components/buttons/SolidButton';
 import { WalletLogo } from '../../components/icons/WalletLogo';
 import Wallet from '../../images/icons/wallet.svg';
 import { useIsSsr } from '../../utils/ssr';
+import { useStore } from '../store';
 
-import { SideBarMenu } from './SideBarMenu';
-import { WalletEnvSelectionModal } from './WalletEnvSelectionModal';
 import { useAccounts, useWalletDetails } from './hooks/multiProtocol';
 
 export function WalletControlBar() {
   const isSsr = useIsSsr();
 
-  const [showEnvSelectModal, setShowEnvSelectModal] = useState(false);
-  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const { setShowEnvSelectModal, setIsSideBarOpen } = useStore((s) => ({
+    setShowEnvSelectModal: s.setShowEnvSelectModal,
+    setIsSideBarOpen: s.setIsSideBarOpen,
+  }));
 
   const { readyAccounts } = useAccounts();
   const walletDetails = useWalletDetails();
@@ -47,7 +48,7 @@ export function WalletControlBar() {
 
         {numReady === 1 && (
           <SolidButton onClick={() => setIsSideBarOpen(true)} classes="px-2.5 py-1" color="white">
-            <div className="flex items-center justify-center">
+            <div className="flex w-36 items-center justify-center xs:w-auto">
               <WalletLogo walletDetails={firstWallet} size={26} />
               <div className="mx-3 flex flex-col items-start">
                 <div className="text-xs text-gray-500">{firstWallet.name || 'Wallet'}</div>
@@ -57,6 +58,7 @@ export function WalletControlBar() {
                     : 'Unknown'}
                 </div>
               </div>
+              <ChevronIcon direction="s" width={10} height={6} />
             </div>
           </SolidButton>
         )}
@@ -74,22 +76,11 @@ export function WalletControlBar() {
                 <div className="text-xs text-gray-500">Wallets</div>
                 <div className="text-xs">{`${numReady} Connected`}</div>
               </div>
+              <ChevronIcon direction="s" width={10} height={6} />
             </div>
           </SolidButton>
         )}
       </div>
-
-      <WalletEnvSelectionModal
-        isOpen={showEnvSelectModal}
-        close={() => setShowEnvSelectModal(false)}
-      />
-      {numReady > 0 && (
-        <SideBarMenu
-          onClose={() => setIsSideBarOpen(false)}
-          isOpen={isSideBarOpen}
-          onConnectWallet={() => setShowEnvSelectModal(true)}
-        />
-      )}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,16 @@ import type { NextPage } from 'next';
 
 import { TipCard } from '../components/tip/TipCard';
 import { TransferTokenCard } from '../features/transfer/TransferTokenCard';
+import { WalletFloatingButtons } from '../features/wallet/WalletFloatingButtons';
 
 const Home: NextPage = () => {
   return (
     <div className="space-y-3 pt-4">
       <TipCard />
-      <TransferTokenCard />
+      <div className="relative">
+        <TransferTokenCard />
+        <WalletFloatingButtons />
+      </div>
     </div>
   );
 };

cfd5fcd085bad94ab9c887abf558abfb3ed5cb87
Trevor Porter <tkporter4@gmail.com>
2024-11-05 12:12:09 +0000

    feat: support native IBC transfers to/from Celestia for Stride/Eclipse warp routes

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -5,7 +5,7 @@ import { TokenConnectionType, TokenStandard, WarpCoreConfig } from '@hyperlane-x
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const warpRouteConfigs: WarpCoreConfig = {
   tokens: [
-    // TIA Celestia to Neutron
+    // TIA Celestia to Neutron and Stride
     {
       chainName: 'celestia',
       standard: TokenStandard.CosmosIbc,
@@ -15,6 +15,7 @@ export const warpRouteConfigs: WarpCoreConfig = {
       addressOrDenom: 'utia',
       logoURI: '/deployments/warp_routes/TIA/logo.svg',
       connections: [
+        // To Neutron
         {
           token:
             'cosmos|neutron|ibc/773B4D0A3CD667B2275D5A4A7A2F0909C0BA0F4059C0B9181E680DDF4965DCC7',
@@ -22,6 +23,7 @@ export const warpRouteConfigs: WarpCoreConfig = {
           sourcePort: 'transfer',
           sourceChannel: 'channel-8',
         },
+        // To Aribtrum via Neutron
         {
           token: 'ethereum|arbitrum|0xD56734d7f9979dD94FAE3d67C7e928234e71cD4C',
           type: TokenConnectionType.IbcHyperlane,
@@ -33,6 +35,7 @@ export const warpRouteConfigs: WarpCoreConfig = {
           intermediateIbcDenom:
             'ibc/773B4D0A3CD667B2275D5A4A7A2F0909C0BA0F4059C0B9181E680DDF4965DCC7',
         },
+        // To Manta Pacific via Neutron
         {
           token: 'ethereum|mantapacific|0x6Fae4D9935E2fcb11fC79a64e917fb2BF14DaFaa',
           type: TokenConnectionType.IbcHyperlane,
@@ -43,6 +46,26 @@ export const warpRouteConfigs: WarpCoreConfig = {
             'neutron1ch7x3xgpnj62weyes8vfada35zff6z59kt2psqhnx9gjnt2ttqdqtva3pa',
           intermediateIbcDenom:
             'ibc/773B4D0A3CD667B2275D5A4A7A2F0909C0BA0F4059C0B9181E680DDF4965DCC7',
+        },
+        // To Stride
+        {
+          token:
+            'cosmos|stride|ibc/BF3B4F53F3694B66E13C23107C84B6485BD2B96296BB7EC680EA77BBA75B4801',
+          type: TokenConnectionType.Ibc,
+          sourcePort: 'transfer',
+          sourceChannel: 'channel-4',
+        },
+        // To Eclipse via Stride
+        {
+          token: 'sealevel|eclipsemainnet|BpXHAiktwjx7fN6M9ST9wr6qKAsH27wZFhdHEhReJsR6',
+          type: TokenConnectionType.IbcHyperlane,
+          sourcePort: 'transfer',
+          sourceChannel: 'channel-4',
+          intermediateChainName: 'stride',
+          intermediateRouterAddress:
+            'stride1pvtesu3ve7qn7ctll2x495mrqf2ysp6fws68grvcu6f7n2ajghgsh2jdj6',
+          intermediateIbcDenom:
+            'ibc/BF3B4F53F3694B66E13C23107C84B6485BD2B96296BB7EC680EA77BBA75B4801',
         },
       ],
     },
@@ -65,6 +88,25 @@ export const warpRouteConfigs: WarpCoreConfig = {
         },
       ],
     },
+
+    // TIA on Stride from Celestia
+    {
+      chainName: 'stride',
+      standard: TokenStandard.CosmosIbc,
+      name: 'Celestia',
+      symbol: 'TIA',
+      decimals: 6,
+      addressOrDenom: 'ibc/BF3B4F53F3694B66E13C23107C84B6485BD2B96296BB7EC680EA77BBA75B4801',
+      logoURI: '/deployments/warp_routes/TIA/logo.svg',
+      connections: [
+        {
+          token: 'cosmos|celestia|utia',
+          type: TokenConnectionType.Ibc,
+          sourcePort: 'transfer',
+          sourceChannel: 'channel-162',
+        },
+      ],
+    },
   ],
   options: {
     interchainFeeConstants: [
@@ -78,6 +120,12 @@ export const warpRouteConfigs: WarpCoreConfig = {
         origin: 'celestia',
         destination: 'mantapacific',
         amount: 270000,
+        addressOrDenom: 'utia',
+      },
+      {
+        origin: 'celestia',
+        destination: 'eclipsemainnet',
+        amount: 120000,
         addressOrDenom: 'utia',
       },
     ],

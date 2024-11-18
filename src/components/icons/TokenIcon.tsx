54c05a7869c4c4d3b4de42de16e47103673c1bcd
J M Rossy <jm.rossy@gmail.com>
2024-11-18 18:29:19 -0500

    Improve token image error handling

--- src/components/icons/TokenIcon.tsx
+++ src/components/icons/TokenIcon.tsx
@@ -2,8 +2,8 @@ import { IRegistry } from '@hyperlane-xyz/registry';
 import { IToken } from '@hyperlane-xyz/sdk';
 import { isHttpsUrl, isRelativeUrl } from '@hyperlane-xyz/utils';
 import { Circle } from '@hyperlane-xyz/widgets';
+import { useState } from 'react';
 import { useStore } from '../../features/store';
-import { ErrorBoundary } from '../errors/ErrorBoundary';
 
 interface Props {
   token?: IToken | null;
@@ -15,17 +15,24 @@ export function TokenIcon({ token, size = 32 }: Props) {
   const character = title ? title.charAt(0).toUpperCase() : '';
   const fontSize = Math.floor(size / 2);
 
+  const [fallbackToText, setFallbackToText] = useState(false);
   const registry = useStore((s) => s.registry);
   const imageSrc = getImageSrc(registry, token);
   const bgColorSeed =
-    token && !imageSrc ? (Buffer.from(token.addressOrDenom).at(0) || 0) % 5 : undefined;
+    token && (!imageSrc || fallbackToText)
+      ? (Buffer.from(token.addressOrDenom).at(0) || 0) % 5
+      : undefined;
 
   return (
     <Circle size={size} bgColorSeed={bgColorSeed} title={title}>
-      {imageSrc ? (
-        <ErrorBoundary hideError={true}>
-          <img src={imageSrc} width={size} height={size} className="p-0.5" />
-        </ErrorBoundary>
+      {imageSrc && !fallbackToText ? (
+        <img
+          src={imageSrc}
+          width={size}
+          height={size}
+          className="p-0.5"
+          onError={() => setFallbackToText(true)}
+        />
       ) : (
         <div className={`text-[${fontSize}px]`}>{character}</div>
       )}

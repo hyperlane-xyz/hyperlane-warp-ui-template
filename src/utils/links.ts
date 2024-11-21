9a150b0cc5ef1565f5d33db7c543eef6ee72e0db
J M Rossy <jm.rossy@gmail.com>
2024-11-21 17:51:23 -0500

    Fix transfer details modal error for successful transfer when metadata is missing

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -4,7 +4,6 @@ import { config } from '../consts/config';
 import { links } from '../consts/links';
 import { isPermissionlessChain } from '../features/chains/utils';
 
-// TODO test with cosmos chain config, or disallow it
 export function getHypExplorerLink(
   multiProvider: MultiProtocolProvider,
   chain: ChainName,
@@ -12,13 +11,15 @@ export function getHypExplorerLink(
 ) {
   if (!config.enableExplorerLink || !chain || !msgId) return null;
   const baseLink = `${links.explorer}/message/${msgId}`;
-  if (isPermissionlessChain(multiProvider, chain)) {
-    const chainMetadata = multiProvider.getChainMetadata(chain);
-    const serializedConfig = toBase64([chainMetadata]);
-    if (serializedConfig) {
-      const params = new URLSearchParams({ chains: serializedConfig });
-      return `${baseLink}?${params.toString()}`;
-    }
-  }
-  return baseLink;
+
+  if (!isPermissionlessChain(multiProvider, chain)) return baseLink;
+
+  const chainMetadata = multiProvider.tryGetChainMetadata(chain);
+  if (!chainMetadata) return baseLink;
+
+  const serializedConfig = toBase64([chainMetadata]);
+  if (!serializedConfig) return baseLink;
+
+  const params = new URLSearchParams({ chains: serializedConfig });
+  return `${baseLink}?${params.toString()}`;
 }

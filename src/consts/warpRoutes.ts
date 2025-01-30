6322075075ea2342d939f92f0171e2c22fc5a9c6
Yuri Visser <yurivisser7@gmail.com>
2025-01-30 08:51:51 -0400

    consolidate tokens & fix connections

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -17,6 +17,9 @@ export const warpRouteConfigs: WarpCoreConfig = {
         {
           token: 'ethereum|treasure|0xb73e4f558F7d4436d77a18f56e4EE9d01764c641',
         },
+        {
+          token: 'ethereum|ethereum|0x53ccE6d10e43d1B3D11872Ad22eC2aCd8d2537B8',
+        },
       ],
     },
     {
@@ -30,16 +33,6 @@ export const warpRouteConfigs: WarpCoreConfig = {
         {
           token: 'ethereum|arbitrum|0xA4Bbac7eD5BdA8Ec71a1aF5ee84d4c5a737bD875',
         },
-      ],
-    },
-    {
-      chainName: 'treasure',
-      standard: TokenStandard.EvmHypSynthetic,
-      decimals: 18,
-      symbol: 'SMOL',
-      name: 'SMOL',
-      addressOrDenom: '0xb73e4f558F7d4436d77a18f56e4EE9d01764c641',
-      connections: [
         {
           token: 'ethereum|ethereum|0x53ccE6d10e43d1B3D11872Ad22eC2aCd8d2537B8',
         },
@@ -56,88 +49,11 @@ export const warpRouteConfigs: WarpCoreConfig = {
         {
           token: 'ethereum|treasure|0xb73e4f558F7d4436d77a18f56e4EE9d01764c641',
         },
-      ],
-    },
-    {
-      chainName: 'arbitrum',
-      standard: TokenStandard.EvmHypSynthetic,
-      decimals: 18,
-      symbol: 'SMOL',
-      name: 'SMOL',
-      addressOrDenom: '0xA4Bbac7eD5BdA8Ec71a1aF5ee84d4c5a737bD875',
-      connections: [
-        {
-          token: 'ethereum|ethereum|0x53ccE6d10e43d1B3D11872Ad22eC2aCd8d2537B8',
-        },
-      ],
-    },
-    {
-      chainName: 'ethereum',
-      standard: TokenStandard.EvmHypSynthetic,
-      decimals: 18,
-      symbol: 'SMOL',
-      name: 'SMOL',
-      addressOrDenom: '0x53ccE6d10e43d1B3D11872Ad22eC2aCd8d2537B8',
-      collateralAddressOrDenom: '0x9E64D3b9e8eC387a9a58CED80b71Ed815f8D82B5',
-      connections: [
         {
           token: 'ethereum|arbitrum|0xA4Bbac7eD5BdA8Ec71a1aF5ee84d4c5a737bD875',
         },
       ],
     },
-    // {
-    //   chainName: 'arbitrumsepolia',
-    //   standard: TokenStandard.EvmHypCollateral,
-    //   decimals: 18,
-    //   symbol: 'SMOL',
-    //   name: 'SMOL',
-    //   addressOrDenom: '0x608238d2F754FA7f4A717d283F135D0cE50C493F',
-    //   collateralAddressOrDenom: '0x82F3f3f5cD3e28B37eFbCFa985b93E82Ea6bE2a3',
-    //   connections: [
-    //     {
-    //       token: 'ethereum|treasuretopaz|0xab72295bc9C74DEe0A30231C486b2D4A2ae0457e',
-    //     },
-    //   ],
-    // },
-    // {
-    //   chainName: 'treasuretopaz',
-    //   standard: TokenStandard.EvmHypNative,
-    //   decimals: 18,
-    //   symbol: 'SMOL',
-    //   name: 'SMOL',
-    //   addressOrDenom: '0xab72295bc9C74DEe0A30231C486b2D4A2ae0457e',
-    //   connections: [
-    //     {
-    //       token: 'ethereum|arbitrumsepolia|0x608238d2F754FA7f4A717d283F135D0cE50C493F',
-    //     },
-    //   ],
-    // },
-    // {
-    //   chainName: 'treasuretopaz',
-    //   standard: TokenStandard.EvmHypSynthetic,
-    //   decimals: 18,
-    //   symbol: 'SMOL',
-    //   name: 'SMOL',
-    //   addressOrDenom: '0xab72295bc9C74DEe0A30231C486b2D4A2ae0457e',
-    //   connections: [
-    //     {
-    //       token: 'ethereum|sepolia|0x544e25bEd12f95B5C0ebf6DA6ADB71F5Bb5773E4',
-    //     },
-    //   ],
-    // },
-    // {
-    //   chainName: 'sepolia',
-    //   standard: TokenStandard.EvmHypSynthetic,
-    //   decimals: 18,
-    //   symbol: 'SMOL',
-    //   name: 'SMOL',
-    //   addressOrDenom: '0x544e25bEd12f95B5C0ebf6DA6ADB71F5Bb5773E4',
-    //   connections: [
-    //     {
-    //       token: 'ethereum|treasuretopaz|0xab72295bc9C74DEe0A30231C486b2D4A2ae0457e',
-    //     },
-    //   ],
-    // },
   ],
   options: {},
 };

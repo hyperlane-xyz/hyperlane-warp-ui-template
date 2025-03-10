8aa8fce39c6e10b6f070878adea711bb254e3c8d
Xaroz <jasonguo2013jg@gmail.com>
2025-03-10 11:30:46 -0400

    chore: add ETH/base-ethereum-lumiaprism route

--- src/consts/warpRouteWhitelist.ts
+++ src/consts/warpRouteWhitelist.ts
@@ -14,6 +14,7 @@ export const warpRouteWhitelist: Array<string> | null = [
   // ETH routes
   'ETH/ethereum-hyperevm',
   'ETH/ethereum-viction',
+  'ETH/base-ethereum-lumiaprism',
 
   // tETH routes
   'tETH/eclipsemainnet-ethereum',

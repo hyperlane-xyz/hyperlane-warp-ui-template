90d8f33d7878227193bf78b74e2f28d08364e3e2
Xaroz <jasonguo2013jg@gmail.com>
2025-03-31 12:12:32 -0400

    chore: initial oUSDT branch setup and config

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -1,30 +1,11 @@
-import {
-  eclipsemainnet,
-  eclipsemainnetAddresses,
-  ethereum,
-  solanamainnet,
-  solanamainnetAddresses,
-} from '@hyperlane-xyz/registry';
-import { ChainMap, ChainMetadata } from '@hyperlane-xyz/sdk';
+import { ink, worldchain } from '@hyperlane-xyz/registry';
+import { ChainDisabledReason, ChainMap, ChainMetadata, ChainStatus } from '@hyperlane-xyz/sdk';
 
 // A map of chain names to ChainMetadata
 // Chains can be defined here, in chains.json, or in chains.yaml
 // Chains already in the SDK need not be included here unless you want to override some fields
 // Schema here: https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/main/typescript/sdk/src/metadata/chainMetadataTypes.ts
 export const chains: ChainMap<ChainMetadata & { mailbox?: Address }> = {
-  solanamainnet: {
-    ...solanamainnet,
-    // SVM chains require mailbox addresses for the token adapters
-    mailbox: solanamainnetAddresses.mailbox,
-    // Including a convenient rpc override because the Solana public RPC does not allow browser requests from localhost
-    rpcUrls: process.env.NEXT_PUBLIC_SOLANA_RPC_URL
-      ? [{ http: process.env.NEXT_PUBLIC_SOLANA_RPC_URL }]
-      : solanamainnet.rpcUrls,
-  },
-  eclipsemainnet: {
-    ...eclipsemainnet,
-    mailbox: eclipsemainnetAddresses.mailbox,
-  },
   // mycustomchain: {
   //   protocol: ProtocolType.Ethereum,
   //   chainId: 123123,
@@ -48,10 +29,18 @@ export const chains: ChainMap<ChainMetadata & { mailbox?: Address }> = {
   //   },
   //   logoURI: '/logo.svg',
   // },
-  ethereum: {
-    ...ethereum,
-    rpcUrls: process.env.NEXT_PUBLIC_ETHEREUM_RPC_URL
-      ? [{ http: process.env.NEXT_PUBLIC_ETHEREUM_RPC_URL }]
-      : ethereum.rpcUrls,
+  ink: {
+    ...ink,
+    availability: {
+      status: ChainStatus.Disabled,
+      reasons: [ChainDisabledReason.Other],
+    },
+  },
+  worldchain: {
+    ...worldchain,
+    availability: {
+      status: ChainStatus.Disabled,
+      reasons: [ChainDisabledReason.Other],
+    },
   },
 };

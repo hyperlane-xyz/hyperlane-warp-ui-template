02c67cfffbe95851673a141642afbfc0b220c82f
Xaroz <jasonguo2013jg@gmail.com>
2025-04-21 18:53:13 -0400

    chore: hyper branding update

--- src/components/tip/TipCard.tsx
+++ src/components/tip/TipCard.tsx
@@ -10,19 +10,19 @@ export function TipCard() {
   if (!show) return null;
   return (
     <Card className="w-100 p-2 sm:w-[31rem]">
-      <h2 className="text-primary-500">Update for MAGIC bridges!</h2>
+      <h2 className="text-primary-500">HYPER is a crosschain asset!</h2>
       <div className="flex items-end justify-between">
         <p className="mt-1 max-w-[75%] text-xs">
-          Please use the official Treasure UI for token transfers between Treasure and Arbitrum
+          Bridge across Arbitrum, Base, BNB Chain, Ethereum, and Optimism.
         </p>
         <a
-          href="https://bridge.treasure.lol/"
+          href="https://docs.hyperlane.xyz/docs/protocol-economics/intro"
           target="_blank"
           rel="noopener noreferrer"
           className="ml-2 flex items-center rounded-lg bg-gray-100 px-3 py-1.5 text-xs text-primary-500 transition-all hover:bg-gray-200 active:bg-gray-300 sm:text-sm"
         >
           <Image src={InfoCircle} width={12} alt="" />
-          <span className="ml-1.5 hidden text-sm sm:inline">Link</span>
+          <span className="ml-1.5 hidden text-sm sm:inline">More</span>
         </a>
       </div>
       <div className="absolute right-3 top-3">

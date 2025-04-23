1b1217abfaf52ca5aeff3d15d574a484cc1fe9d1
nicosampler <nf.dominguez.87@gmail.com>
2025-04-23 15:49:03 -0300

    feat: add transfer extra steps for khalani

--- src/features/transfer/useTokenTransfer.ts
+++ src/features/transfer/useTokenTransfer.ts
@@ -1,9 +1,16 @@
-import { TypedTransactionReceipt, WarpCore, WarpTxCategory } from '@hyperlane-xyz/sdk';
-import { toTitleCase, toWei } from '@hyperlane-xyz/utils';
+import {
+  EvmKhalaniHypAdapter,
+  TokenStandard,
+  TypedTransactionReceipt,
+  WarpCore,
+  WarpTxCategory,
+} from '@hyperlane-xyz/sdk';
+import { fromWei, toTitleCase, toWei } from '@hyperlane-xyz/utils';
 import {
   getAccountAddressForChain,
   useAccounts,
   useActiveChains,
+  useEthereumSignTypedData,
   useTransactionFns,
 } from '@hyperlane-xyz/widgets';
 import { useCallback, useState } from 'react';
@@ -35,6 +42,7 @@ export function useTokenTransfer(onDone?: () => void) {
   const activeAccounts = useAccounts(multiProvider);
   const activeChains = useActiveChains(multiProvider);
   const transactionFns = useTransactionFns(multiProvider);
+  const { signTypedData } = useEthereumSignTypedData(multiProvider);
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -52,6 +60,7 @@ export function useTokenTransfer(onDone?: () => void) {
         updateTransferStatus,
         setIsLoading,
         onDone,
+        signTypedData,
       }),
     [
       warpCore,
@@ -63,6 +72,7 @@ export function useTokenTransfer(onDone?: () => void) {
       addTransfer,
       updateTransferStatus,
       onDone,
+      signTypedData,
     ],
   );
 
@@ -83,6 +93,7 @@ async function executeTransfer({
   updateTransferStatus,
   setIsLoading,
   onDone,
+  signTypedData,
 }: {
   warpCore: WarpCore;
   values: TransferFormValues;
@@ -94,6 +105,7 @@ async function executeTransfer({
   updateTransferStatus: AppState['updateTransferStatus'];
   setIsLoading: (b: boolean) => void;
   onDone?: () => void;
+  signTypedData: ReturnType<typeof useEthereumSignTypedData>['signTypedData'];
 }) {
   logger.debug('Preparing transfer transaction(s)');
   setIsLoading(true);
@@ -150,6 +162,7 @@ async function executeTransfer({
 
     const hashes: string[] = [];
     let txReceipt: TypedTransactionReceipt | undefined = undefined;
+
     for (const tx of txs) {
       updateTransferStatus(transferIndex, (transferStatus = txCategoryToStatuses[tx.category][0]));
       const { hash, confirm } = await sendTransaction({
@@ -165,14 +178,96 @@ async function executeTransfer({
       hashes.push(hash);
     }
 
-    const msgId = txReceipt
-      ? tryGetMsgIdFromTransferReceipt(multiProvider, origin, txReceipt)
-      : undefined;
+    if (originToken.standard === TokenStandard.EvmKhalaniIntent) {
+      const adapter = originTokenAmount.token.getHypAdapter(multiProvider) as EvmKhalaniHypAdapter;
+      const toChainId = multiProvider.getChainId(connection.token.chainName);
+      const refineId = await adapter.createRefine(sender, Number(toChainId), weiAmountOrId);
 
-    updateTransferStatus(transferIndex, (transferStatus = TransferStatus.ConfirmedTransfer), {
-      originTxHash: hashes.at(-1),
-      msgId,
-    });
+      // RefineResult contains the intent data
+      type RefineResult = Awaited<ReturnType<typeof adapter.queryRefine>>;
+      let refineData: RefineResult = {} as RefineResult;
+
+      // wait for user signature
+      // fee is updated until the user signs the intent
+      let isSigning = false;
+      let isSigned = false;
+      let signedIntentSignature: string | undefined = undefined;
+      while (!isSigned) {
+        try {
+          if (!isSigning) {
+            refineData = await adapter.queryRefine(refineId);
+            updateTransferStatus(transferIndex, TransferStatus.SigningMessage, {
+              signingMessage: {
+                message:
+                  `Sending ${fromWei(refineData.Refinement.outcome.mAmounts[0], connection.token.decimals)} ${originToken.symbol}\n` +
+                  `to: ${destination}\n\n` +
+                  `Sign with your wallet to create and send intent.`,
+                handleSign: async () => {
+                  isSigning = true;
+                  const signPayload = await adapter.buildIntentSigningPayload(refineData, sender);
+                  return signTypedData(signPayload as any, origin, activeChain.chainName)
+                    .then(async (signature) => {
+                      isSigned = true;
+                      signedIntentSignature = signature;
+                      toast.success('Intent signed successfully');
+                      updateTransferStatus(transferIndex, TransferStatus.ProcessingDeposit);
+                    })
+                    .catch(() => {
+                      isSigning = false;
+                      toast.error('Error signing Intent. Please try again.');
+                    });
+                },
+              },
+            });
+          }
+        } catch (error) {
+          logger.error('Error checking for signature request:', error);
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+      }
+
+      // Wait for mToken minting on Arcadia chain.
+      await adapter.waitForMTokenMinting(BigInt(weiAmountOrId), sender);
+
+      // propose intent
+      if (!signedIntentSignature) throw new Error('No intent signature found');
+      const { intentId, transactionHash } = await adapter.proposeIntent(
+        refineData as RefineResult,
+        signedIntentSignature,
+      );
+      updateTransferStatus(transferIndex, TransferStatus.WaitingForFulfillment);
+
+      // wait for intent to be settled
+      let isIntentCompleted = false;
+      while (!isIntentCompleted) {
+        try {
+          const status = await adapter.getIntentStatus(intentId);
+          if (status === 'Solved') {
+            isIntentCompleted = true;
+
+            updateTransferStatus(transferIndex, TransferStatus.ConfirmedTransfer, {
+              providerExplorerLink: `https://explorer.khalani.network/tx/${transactionHash}`,
+            });
+          } else if (status === 'NonExistent' || status === 'Expired' || status === 'Cancelled') {
+            isIntentCompleted = true;
+            updateTransferStatus(transferIndex, TransferStatus.Failed);
+          }
+          await new Promise((resolve) => setTimeout(resolve, 2000));
+        } catch (error) {
+          logger.error('Error checking intent status:', error);
+        }
+      }
+    } else {
+      const msgId = txReceipt
+        ? tryGetMsgIdFromTransferReceipt(multiProvider, origin, txReceipt)
+        : undefined;
+
+      updateTransferStatus(transferIndex, (transferStatus = TransferStatus.ConfirmedTransfer), {
+        originTxHash: hashes.at(-1),
+        msgId,
+      });
+    }
   } catch (error: any) {
     logger.error(`Error at stage ${transferStatus}`, error);
     const errorDetails = error.message || error.toString();

75abe2776f2d75f3d0d5614eee3f75baab44a398
Xaroz <jasonguo2013jg@gmail.com>
2025-05-07 10:53:34 -0400

    chore: add ethereum to tip card, refactor set function

--- src/components/tip/TipCard.tsx
+++ src/components/tip/TipCard.tsx
@@ -1,6 +1,7 @@
 import { useFormikContext } from 'formik';
 import { CHAIN_NAMES, WARP_QUERY_PARAMS } from '../../consts/args';
 import { config } from '../../consts/config';
+import { useStore } from '../../features/store';
 import { getTokenByIndex, getTokenIndexFromChains, useWarpCore } from '../../features/tokens/hooks';
 import { TransferFormValues } from '../../features/transfer/types';
 import { updateQueryParam } from '../../utils/queryParams';
@@ -9,17 +10,14 @@ import { Card } from '../layout/Card';
 export function TipCard() {
   const warpCore = useWarpCore();
   const { values, setFieldValue } = useFormikContext<TransferFormValues>();
+  const setOriginChainName = useStore((s) => s.setOriginChainName);
 
-  const handleSetCeloChain = () => {
-    setFieldValue(WARP_QUERY_PARAMS.ORIGIN, CHAIN_NAMES.CELO);
-    updateQueryParam(WARP_QUERY_PARAMS.ORIGIN, CHAIN_NAMES.CELO);
+  const handleSetOriginChain = (chainName: string) => {
+    setFieldValue(WARP_QUERY_PARAMS.ORIGIN, chainName);
+    updateQueryParam(WARP_QUERY_PARAMS.ORIGIN, chainName);
+    setOriginChainName(chainName);
 
-    const tokenIndex = getTokenIndexFromChains(
-      warpCore,
-      null,
-      CHAIN_NAMES.CELO,
-      values.destination,
-    );
+    const tokenIndex = getTokenIndexFromChains(warpCore, null, chainName, values.destination);
     const token = getTokenByIndex(warpCore, tokenIndex);
     updateQueryParam(WARP_QUERY_PARAMS.TOKEN, token?.addressOrDenom);
     setFieldValue('tokenIndex', tokenIndex);
@@ -32,7 +30,16 @@ export function TipCard() {
       <h2 className="text-primary-500">Get OpenUSDT!</h2>
       <div className="flex items-end justify-between">
         <ul className="mt-1 list-disc text-xs">
-          <li className={`${styles.list} cursor-pointer`} onClick={handleSetCeloChain}>
+          <li
+            className={`${styles.list} cursor-pointer`}
+            onClick={() => handleSetOriginChain(CHAIN_NAMES.ETHEREUM)}
+          >
+            Send USDT From Ethereum to get OpenUSDT on other chains
+          </li>
+          <li
+            className={`${styles.list} cursor-pointer`}
+            onClick={() => handleSetOriginChain(CHAIN_NAMES.CELO)}
+          >
             Send USDT From Celo to get OpenUSDT on other chains
           </li>
         </ul>

477f6cdb4310617bd111c57caed22c8afc301372
vish-han <vshlchauhab@gmail.com>
2025-05-26 12:44:00 +0530

    Chains and routes added

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -1,3 +1,4 @@
+
 import {
   eclipsemainnet,
   eclipsemainnetAddresses,
@@ -8,7 +9,9 @@ import {
   soon,
   soonAddresses,
 } from '@hyperlane-xyz/registry';
-import { ChainMap, ChainMetadata } from '@hyperlane-xyz/sdk';
+import { ChainMap, ChainMetadata, ExplorerFamily } from '@hyperlane-xyz/sdk';
+import { Address, ProtocolType } from '@hyperlane-xyz/utils';
+
 
 // A map of chain names to ChainMetadata
 // Chains can be defined here, in chains.json, or in chains.yaml
@@ -32,29 +35,78 @@ export const chains: ChainMap<ChainMetadata & { mailbox?: Address }> = {
     ...sonicsvm,
     mailbox: sonicsvmAddresses.mailbox,
   },
-  // mycustomchain: {
-  //   protocol: ProtocolType.Ethereum,
-  //   chainId: 123123,
-  //   domainId: 123123,
-  //   name: 'mycustomchain',
-  //   displayName: 'My Chain',
-  //   nativeToken: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  //   rpcUrls: [{ http: 'https://mycustomchain-rpc.com' }],
-  //   blockExplorers: [
-  //     {
-  //       name: 'MyCustomScan',
-  //       url: 'https://mycustomchain-scan.com',
-  //       apiUrl: 'https://api.mycustomchain-scan.com/api',
-  //       family: ExplorerFamily.Etherscan,
-  //     },
-  //   ],
-  //   blocks: {
-  //     confirmations: 1,
-  //     reorgPeriod: 1,
-  //     estimateBlockTime: 10,
-  //   },
-  //   logoURI: '/logo.svg',
-  // },
+  basesepolia: {
+    protocol: ProtocolType.Ethereum,
+    chainId: 84532,
+    domainId: 84532,
+    name: 'basesepolia',
+    displayName: 'Base Sepolia',
+    nativeToken: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+    rpcUrls: [{ http: 'https://sepolia.base.org' }],
+    blockExplorers: [
+      {
+        name: 'BaseScan',
+        url: 'https://sepolia.basescan.org',
+        apiUrl: 'https://api-sepolia.basescan.org/api',
+        family: ExplorerFamily.Etherscan,
+      },
+    ],
+    blocks: {
+      confirmations: 1,
+      reorgPeriod: 1,
+      estimateBlockTime: 2,
+    },
+    mailbox: '0x6966b0E55883d49BFB24539356a2f8A673E02039',
+    logoURI: '/base-logo.svg',
+  },
+  bsctestnet: {
+    protocol: ProtocolType.Ethereum,
+    chainId: 97,
+    domainId: 97,
+    name: 'bsctestnet',
+    displayName: 'BSC Testnet',
+    nativeToken: { name: 'BNB', symbol: 'BNB', decimals: 18 },
+    rpcUrls: [{ http: 'https://data-seed-prebsc-1-s1.binance.org:8545' }],
+    blockExplorers: [
+      {
+        name: 'BscScan',
+        url: 'https://testnet.bscscan.com',
+        apiUrl: 'https://api-testnet.bscscan.com/api',
+        family: ExplorerFamily.Etherscan,
+      },
+    ],
+    blocks: {
+      confirmations: 1,
+      reorgPeriod: 9,
+      estimateBlockTime: 3,
+    },
+    mailbox: '0xF9F6F5646F478d5ab4e20B0F910C92F1CCC9Cc6D',
+    logoURI: '/bsc-logo.svg',
+  },
+  edgentestnet: {
+    protocol: ProtocolType.Ethereum,
+    chainId: 3456,
+    domainId: 3456,
+    name: 'edgentestnet',
+    displayName: 'Edge Testnet',
+    nativeToken: { name: 'EDGEN', symbol: 'EDGEN', decimals: 18 },
+    rpcUrls: [{ http: 'https://testnet-rpc.layeredge.io' }],
+    blockExplorers: [
+      {
+        name: 'EdgeExplorer',
+        url: 'https://testnet-explorer.layeredge.io',
+        apiUrl: 'https://api.explorer.edgenet.example.com/api',
+        family: ExplorerFamily.Blockscout,
+      },
+    ],
+    blocks: {
+      confirmations: 1,
+      reorgPeriod: 1,
+      estimateBlockTime: 2,
+    },
+    mailbox: '0x038f2E7Fcbac0BBEC0D1101Ee17029C5B0b7eCFD',
+    logoURI: '/edge-logo.svg',
+  },
 };
 
 // rent account payment for (mostly for) SVM chains added on top of IGP,
--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,9 +1,60 @@
-import { WarpCoreConfig } from '@hyperlane-xyz/sdk';
+import { TokenStandard, WarpCoreConfig } from '@hyperlane-xyz/sdk';
+
 
 // A list of Warp Route token configs
 // These configs will be merged with the warp routes in the configured registry
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const warpRouteConfigs: WarpCoreConfig = {
-  tokens: [],
+  tokens: [
+    {
+      chainName: 'basesepolia',
+      standard: TokenStandard.EvmHypCollateral,
+      decimals: 18,
+      symbol: 'EDGEN',
+      name: 'LayerEdge',
+      addressOrDenom: '0x64508Ee3af87a7F26D4F531B10E13585dA29f15b', // Hyperlane token contract on Base Sepolia
+      collateralAddressOrDenom: '0x64508Ee3af87a7F26D4F531B10E13585dA29f15b', // Underlying token (same as hyperlane token in this case)
+      connections: [
+        {
+          token: 'ethereum|edgentestnet|0xEf140B32A484259b9B0Bad46A5Fe26726B2019Fd',
+        },
+        {
+          token: 'ethereum|bsctestnet|0x83c2084CC653c5149b3f429503188836fe280e71',
+        },
+      ],
+    },
+    {
+      chainName: 'bsctestnet',
+      standard: TokenStandard.EvmHypSynthetic,
+      decimals: 18,
+      symbol: 'EDGEN',
+      name: 'LayerEdge',
+      addressOrDenom: '0x83c2084CC653c5149b3f429503188836fe280e71', // Hyperlane synthetic token on BSC Testnet
+      connections: [
+        {
+          token: 'ethereum|edgentestnet|0xEf140B32A484259b9B0Bad46A5Fe26726B2019Fd',
+        },
+        {
+          token: 'ethereum|basesepolia|0x64508Ee3af87a7F26D4F531B10E13585dA29f15b', // Actual basesepolia token address
+        },
+      ],
+    },
+    {
+      chainName: 'edgentestnet',
+      standard: TokenStandard.EvmHypNative,
+      decimals: 18,
+      symbol: 'EDGEN',
+      name: 'EDGEN',
+      addressOrDenom: '0xEf140B32A484259b9B0Bad46A5Fe26726B2019Fd', // Hyperlane native token on Edge Testnet
+      connections: [
+        {
+          token: 'ethereum|basesepolia|0x64508Ee3af87a7F26D4F531B10E13585dA29f15b', // Actual basesepolia token address
+        },
+        {
+          token: 'ethereum|bsctestnet|0x83c2084CC653c5149b3f429503188836fe280e71',
+        },
+      ],
+    },
+  ],
   options: {},
 };

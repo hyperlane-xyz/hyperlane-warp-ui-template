959db7f20c2587fa126cabbe599732a03743c07e
Xaroz <jasonguo2013jg@gmail.com>
2025-05-30 17:16:21 -0400

    chore: uppercase test IDs

--- src/consts/warpRouteWhitelist.test.ts
+++ src/consts/warpRouteWhitelist.test.ts
@@ -1,10 +1,13 @@
 import { warpRouteConfigs } from '@hyperlane-xyz/registry';
+import { objKeys } from '@hyperlane-xyz/utils';
 import { assert, test } from 'vitest';
 import { warpRouteWhitelist } from './warpRouteWhitelist';
 
 test('warpRouteWhitelist', () => {
   if (!warpRouteWhitelist) return;
+
+  const uppercaseConfigKeys = new Set(objKeys(warpRouteConfigs).map((key) => key.toUpperCase()));
   for (const id of warpRouteWhitelist) {
-    assert(warpRouteConfigs[id], `No route with id ${id} found in registry.`);
+    assert(uppercaseConfigKeys.has(id.toUpperCase()), `No route with id ${id} found in registry.`);
   }
 });

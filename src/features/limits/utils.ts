f66fc81b27f2d48cadbfc1c8949dbb01f982d435
Xaroz <jasonguo2013jg@gmail.com>
2025-06-04 17:38:21 -0400

    chore: add tests, refactor create mocks

--- src/features/limits/utils.ts
+++ src/features/limits/utils.ts
@@ -1,15 +1,22 @@
 import { IToken, Token } from '@hyperlane-xyz/sdk';
+import { logger } from '../../utils/logger';
 import { isValidMultiCollateralToken } from '../tokens/utils';
 import { multiCollateralTokenLimits } from './const';
+import { RouteLimit } from './types';
 
-function getMultiCollateralTokenLimit(originToken: Token | IToken, destination: ChainName) {
+export function getMultiCollateralTokenLimit(
+  originToken: Token | IToken,
+  destination: ChainName,
+  routeLimits: RouteLimit[] = multiCollateralTokenLimits,
+) {
   const destinationToken = originToken.getConnectionForChain(destination)?.token;
   if (!destinationToken) return null;
 
   const isMultiCollateralToken = isValidMultiCollateralToken(originToken, destinationToken);
   if (!isMultiCollateralToken) return null;
+  logger.debug('hello', destinationToken);
 
-  const limitExists = multiCollateralTokenLimits.find((limit) => {
+  const limitExists = routeLimits.find((limit) => {
     if (limit.symbol !== originToken.symbol || limit.symbol !== destinationToken.symbol)
       return false;
 
@@ -29,10 +36,10 @@ export function isMultiCollateralLimitExceeded(
   originToken: Token | IToken,
   destination: ChainName,
   amountWei: string,
+  routeLimits: RouteLimit[] = multiCollateralTokenLimits,
 ): bigint | null {
-  const limitExists = getMultiCollateralTokenLimit(originToken, destination);
+  const limitExists = getMultiCollateralTokenLimit(originToken, destination, routeLimits);
 
-  console.log('limitsExist', limitExists);
   if (!limitExists) return null;
 
   return BigInt(amountWei) > limitExists.amountWei ? limitExists.amountWei : null;

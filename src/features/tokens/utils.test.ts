2da49c3ef242952bc023f697864b6ebf99767ee1
Xaroz <jasonguo2013jg@gmail.com>
2025-06-04 14:01:04 -0400

    chore: better test descriptions

--- src/features/tokens/utils.test.ts
+++ src/features/tokens/utils.test.ts
@@ -47,43 +47,43 @@ beforeEach(() => {
 });
 
 describe('isValidMultiCollateralToken', () => {
-  test('returns false if originToken has no collateralAddressOrDenom', () => {
+  test('should return false if originToken has no collateralAddressOrDenom', () => {
     const token = createMockToken({ collateralAddressOrDenom: undefined });
     expect(isValidMultiCollateralToken(token, 'destination')).toBe(false);
   });
 
-  test('returns false if originToken is not collateralized', () => {
+  test('should return false if originToken is not collateralized', () => {
     const token = createMockToken({ standard: TokenStandard.CosmosIbc });
     expect(isValidMultiCollateralToken(token, 'destination')).toBe(false);
   });
 
-  test('returns false if destinationToken is not found via chain name', () => {
+  test('should return false if destinationToken is not found via chain name', () => {
     const token = createMockToken({ connections: [createTokenConnectionMock()] });
     expect(isValidMultiCollateralToken(token, 'destination')).toBe(false);
   });
 
-  test('returns false if destinationToken has no collateralAddressOrDenom', () => {
+  test('should return false if destinationToken has no collateralAddressOrDenom', () => {
     const token = createMockToken({
       connections: [createTokenConnectionMock(undefined, { collateralAddressOrDenom: undefined })],
     });
     expect(isValidMultiCollateralToken(token, TestChainName.test2)).toBe(false);
   });
 
-  test('returns false if destinationToken is not collateralized', () => {
+  test('should return false if destinationToken is not collateralized', () => {
     const token = createMockToken({
       connections: [createTokenConnectionMock(undefined, { standard: TokenStandard.CosmosIbc })],
     });
     expect(isValidMultiCollateralToken(token, TestChainName.test2)).toBe(false);
   });
 
-  test('returns true when tokens are valid with destinationToken as a string', () => {
+  test('should return true when tokens are valid with destinationToken as a string', () => {
     const token = createMockToken({
       connections: [createTokenConnectionMock()],
     });
     expect(isValidMultiCollateralToken(token, TestChainName.test2)).toBe(true);
   });
 
-  test('returns true when tokens are valid with destinationToken as a IToken', () => {
+  test('should return true when tokens are valid with destinationToken as a IToken', () => {
     const token = createMockToken({
       connections: [createTokenConnectionMock()],
     });

c049ce9950b2a265d710f5726d6b03c5e085b447
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2025-06-10 15:38:59 +0100

    feat: extend ousdt to bob+hashkey (#627)

--- src/consts/warpRouteWhitelist.ts
+++ src/consts/warpRouteWhitelist.ts
@@ -2,6 +2,4 @@
 // Warp Route IDs use format `SYMBOL/chainname1-chainname2...` where chains are ordered alphabetically
 // If left null, all warp routes in the configured registry will be included
 // If set to a list (including an empty list), only the specified routes will be included
-export const warpRouteWhitelist: Array<string> | null = [
-  'oUSDT/base-bitlayer-celo-ethereum-fraxtal-ink-linea-lisk-mantle-metal-metis-mode-optimism-ronin-soneium-sonic-superseed-unichain-worldchain',
-];
+export const warpRouteWhitelist: Array<string> | null = ['oUSDT/production'];

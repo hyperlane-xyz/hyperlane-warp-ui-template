afe19358a96a7c5942d23ae5203bf00cecad1657
Xaroz <jasonguo2013jg@gmail.com>
2025-06-18 16:50:57 -0400

    chore: remove testing limits

--- src/features/limits/const.ts
+++ src/features/limits/const.ts
@@ -1,9 +1,3 @@
 import { RouteLimit } from './types';
 
-export const multiCollateralTokenLimits: RouteLimit[] = [
-  {
-    amountWei: 1000000n, // 1 USDC
-    symbol: 'USDC',
-    chains: ['optimism', 'arbitrum'],
-  },
-];
+export const multiCollateralTokenLimits: RouteLimit[] = [];

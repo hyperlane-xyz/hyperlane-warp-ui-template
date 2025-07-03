ddde176b42778ef6b8ce23eb15bc39ab7a1a3112
Xaroz <jasonguo2013jg@gmail.com>
2025-07-03 11:44:29 -0400

    chore: add limits to USDC route

--- src/features/limits/const.ts
+++ src/features/limits/const.ts
@@ -1,3 +1,9 @@
 import { RouteLimit } from './types';
 
-export const multiCollateralTokenLimits: RouteLimit[] = [];
+export const multiCollateralTokenLimits: RouteLimit[] = [
+  {
+    amountWei: 1000000000n,
+    chains: ['arbitrum', 'base', 'ethereum', 'optimism'],
+    symbol: 'USDC',
+  },
+];

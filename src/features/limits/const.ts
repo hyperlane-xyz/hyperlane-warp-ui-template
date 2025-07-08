29cf8f77e51904cde320a9fb3ed25ca25e868c97
Xaroz <jasonguo2013jg@gmail.com>
2025-07-08 16:06:36 -0400

    fix: comment

--- src/features/limits/const.ts
+++ src/features/limits/const.ts
@@ -2,7 +2,7 @@ import { RouteLimit } from './types';
 
 export const multiCollateralTokenLimits: RouteLimit[] = [
   {
-    amountWei: 5000000000n, // 1000 USDC
+    amountWei: 5000000000n, // 5000 USDC
     chains: ['arbitrum', 'base', 'ethereum', 'optimism'],
     symbol: 'USDC',
   },

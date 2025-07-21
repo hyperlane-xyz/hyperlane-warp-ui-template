975b8549a49035d97d3de5c8b72640140c44bff2
Nam Chu Hoai <nambrot@googlemail.com>
2025-07-21 12:57:04 -0400

    Bump limit to 30k USDC

--- src/features/limits/const.ts
+++ src/features/limits/const.ts
@@ -2,7 +2,7 @@ import { RouteLimit } from './types';
 
 export const multiCollateralTokenLimits: RouteLimit[] = [
   {
-    amountWei: 10000000000n, // 10000 USDC
+    amountWei: 30000000000n, // 30000 USDC
     chains: ['arbitrum', 'base', 'ethereum', 'optimism', 'solanamainnet'],
     symbol: 'USDC',
   },

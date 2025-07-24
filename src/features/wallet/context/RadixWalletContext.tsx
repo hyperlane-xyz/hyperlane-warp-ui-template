7216e7fffe8c6641e19bfb0b798cbd97d232778e
Troy Kessler <troy.kessler99@gmail.com>
2025-07-24 09:52:17 +0200

    chore: add radix wallet connection

---
+++ src/features/wallet/context/RadixWalletContext.tsx
@@ -0,0 +1,33 @@
+import {
+  AccountProvider,
+  GatewayApiProvider,
+  PopupProvider,
+  RdtProvider,
+} from '@hyperlane-xyz/widgets';
+import '@interchain-ui/react/styles';
+import { GatewayApiClient } from '@radixdlt/babylon-gateway-api-sdk';
+import { RadixDappToolkit, RadixNetwork } from '@radixdlt/radix-dapp-toolkit';
+import { PropsWithChildren } from 'react';
+
+export function RadixWalletContext({ children }: PropsWithChildren<unknown>) {
+  const rdt = RadixDappToolkit({
+    networkId: RadixNetwork.Mainnet,
+    applicationVersion: '1.0.0',
+    applicationName: 'Radix Web3 dApp',
+    applicationDappDefinitionAddress:
+      'account_rdx129zzrj4mwjwec8e6rmsvcz0hx4lp7uj3kf73w8rd2fek4cryaemewh',
+    useCache: false,
+  });
+
+  const gatewayApi = GatewayApiClient.initialize(rdt.gatewayApi.clientConfig);
+
+  return (
+    <RdtProvider value={rdt}>
+      <GatewayApiProvider value={gatewayApi}>
+        <AccountProvider>
+          <PopupProvider>{children}</PopupProvider>
+        </AccountProvider>
+      </GatewayApiProvider>
+    </RdtProvider>
+  );
+}

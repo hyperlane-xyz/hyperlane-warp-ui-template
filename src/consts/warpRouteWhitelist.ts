fae562b6da99ff9a645719bf93e63694bf5c6a80
Trevor Porter <tkporter4@gmail.com>
2025-07-28 12:41:04 +0100

    add eth too

--- src/consts/warpRouteWhitelist.ts
+++ src/consts/warpRouteWhitelist.ts
@@ -3,7 +3,7 @@
 // If left null, all warp routes in the configured registry will be included
 // If set to a list (including an empty list), only the specified routes will be included
 export const warpRouteWhitelist: Array<string> | null = [
-  // 'ETH/sepolia-starknetsepolia',
+  'ETH/sepolia-starknetsepolia',
 
   // // TIA routes
   // 'TIA/arbitrum-neutron',

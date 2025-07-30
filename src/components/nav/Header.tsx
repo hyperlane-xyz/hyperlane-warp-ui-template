47232cdc6bd43ada21c25cc94bb5348a42c0934d
derryld3 <derryl@d3labs.io>
2025-07-30 11:38:15 +0700

    change header to pruv logo (#22)

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -1,14 +1,14 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { ConnectWalletButton } from '../../features/wallet/ConnectWalletButton';
+import Logo from '../../images/logos/app-logo.svg';
 
 export function Header() {
   return (
     <header className="w-full px-2 pb-2 pt-3 sm:px-6 lg:px-12">
       <div className="flex items-start justify-between">
         <Link href="/" className="flex items-center py-2">
-          <span className="text-3xl font-bold text-white transition-colors hover:text-gray-200">
-            Pruv Bridge
-          </span>
+          <Image src={Logo} width={128} alt="" />
         </Link>
         <div className="flex flex-col items-end gap-2 md:flex-row-reverse md:items-start">
           <ConnectWalletButton />

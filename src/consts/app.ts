1a394c9ff3a967bd8e4ac6751cc3ec9d0d723e88
Derryl <derryl@d3labs.io>
2025-07-31 16:16:44 +0700

    change url

--- src/consts/app.ts
+++ src/consts/app.ts
@@ -9,7 +9,7 @@ export const MAIN_FONT = Rubik({
 });
 export const APP_NAME = 'Pruv Bridge';
 export const APP_DESCRIPTION = 'A DApp for token bridge transfers';
-export const APP_URL = 'hyperlane-warp-template.vercel.app';
+export const APP_URL = 'bridge.pruv.network';
 export const BRAND_COLOR = Color.primary['500'];
 export const BACKGROUND_COLOR = Color.primary['500'];
 export const BACKGROUND_IMAGE = 'url(/backgrounds/main.svg)';

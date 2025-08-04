3de723b475acc0cb19f39ae83f01cff80c0763f4
Xaroz <jasonguo2013jg@gmail.com>
2025-08-04 13:04:17 -0400

    chore: remove link

--- src/components/tip/TipCard.tsx
+++ src/components/tip/TipCard.tsx
@@ -11,18 +11,9 @@ export function TipCard() {
       <h2 className="text-primary-500">Update for inEVM/injective route</h2>
       <div className="flex items-end justify-between">
         <p className="mt-1 max-w-[75%] text-xs">
-          Bridging INJ between inEVM and Injective is temporarily disabled while our team
+          Bridging INJ between inEVM and Injective is temporarily disabled while the team
           investigates an ongoing issue.
         </p>
-        {/* <a
-          href={links.github}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="ml-2 flex items-center rounded-lg bg-gray-100 px-3 py-1.5 text-xs text-primary-500 transition-all hover:bg-gray-200 active:bg-gray-300 sm:text-sm"
-        >
-          <Image src={InfoCircle} width={12} alt="" />
-          <span className="ml-1.5 hidden text-sm sm:inline">More</span>
-        </a> */}
       </div>
       <div className="absolute right-3 top-3">
         <IconButton onClick={() => setShow(false)} title="Hide tip" className="hover:rotate-90">

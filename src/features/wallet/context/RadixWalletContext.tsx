010f47f4ad91122952eab93f444ede3be800aa37
Jamin <kaminund@gmail.com>
2025-09-05 12:31:29 +0200

    chore: update dependencies

--- src/features/wallet/context/RadixWalletContext.tsx
+++ src/features/wallet/context/RadixWalletContext.tsx
@@ -13,10 +13,10 @@ export function RadixWalletContext({ children }: PropsWithChildren<unknown>) {
   // TODO: RADIX
   // get real dapp definition address
   const rdt = RadixDappToolkit({
-    networkId: RadixNetwork.Stokenet,
+    networkId: RadixNetwork.Mainnet,
     applicationVersion: '1.0.0',
     applicationName: 'Hyperlane Test',
-    dAppDefinitionAddress: 'account_tdx_2_128l0zrskd4ed4hyee2zw5u3jqry5k939kyn359ltmungvsxn2jxd3d',
+    dAppDefinitionAddress: 'account_rdx12xa2c277wq35qk0sruvtd62t2c4mgvewam2px5jsjt4hlmh6r4cwcm',
     useCache: false,
   });
 

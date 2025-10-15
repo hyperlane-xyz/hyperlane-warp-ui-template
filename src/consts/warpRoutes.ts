948618f9de4a6628b087a5ad06a5bf9ecedb7077
nambrot <nambrot@googlemail.com>
2025-10-14 22:00:37 -0400

    Add bsc route

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -19,11 +19,29 @@ export const warpRouteConfigs: WarpCoreConfig = {
         {
           token: 'ethereum|linea|0x36f586A30502AE3afb555b8aA4dCc05d233c2ecE',
         },
+        {
+          token: 'ethereum|bsc|0x36f586A30502AE3afb555b8aA4dCc05d233c2ecE',
+        },
       ],
     },
     // M0 mUSD on Linea (SpokePortal)
     {
       chainName: 'linea',
+      standard: TokenStandard.EvmM0PortalLite,
+      addressOrDenom: '0x36f586A30502AE3afb555b8aA4dCc05d233c2ecE', // Portal address (same as Ethereum)
+      collateralAddressOrDenom: '0xaca92e438df0b2401ff60da7e4337b687a2435da', // mUSD token address
+      decimals: 6,
+      symbol: 'mUSD',
+      name: 'MetaMask USD',
+      logoURI: '/logos/musd.svg',
+      connections: [
+        {
+          token: 'ethereum|ethereum|0x36f586A30502AE3afb555b8aA4dCc05d233c2ecE',
+        },
+      ],
+    },
+    {
+      chainName: 'bsc',
       standard: TokenStandard.EvmM0PortalLite,
       addressOrDenom: '0x36f586A30502AE3afb555b8aA4dCc05d233c2ecE', // Portal address (same as Ethereum)
       collateralAddressOrDenom: '0xaca92e438df0b2401ff60da7e4337b687a2435da', // mUSD token address

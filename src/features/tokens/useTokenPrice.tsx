4d6e3ca8a711ba684abbec481e79aa11e169215f
Xaroz <jasonguo2013jg@gmail.com>
2025-10-30 11:14:02 -0400

    chore: set staleTime for caching

--- src/features/tokens/useTokenPrice.tsx
+++ src/features/tokens/useTokenPrice.tsx
@@ -1,22 +1,25 @@
-import { WarpCore } from '@hyperlane-xyz/sdk';
-import { isNullish } from '@hyperlane-xyz/utils';
+import { Token } from '@hyperlane-xyz/sdk';
 import { useQuery } from '@tanstack/react-query';
 import { logger } from '../../utils/logger';
 import { TransferFormValues } from '../transfer/types';
 import { getTokenByIndex, useWarpCore } from './hooks';
 
-const TOKEN_PRICE_REFRESH_INTERVAL = 30_000; // 30s
+const TOKEN_PRICE_REFRESH_INTERVAL = 60_000; // 60s
 
 export function useTokenPrice({ tokenIndex }: TransferFormValues) {
   const warpCore = useWarpCore();
+  const originToken = getTokenByIndex(warpCore, tokenIndex);
 
   const { data, isError, isLoading } = useQuery({
     // The WarpCore class is not serializable, so we can't use it as a key
     // eslint-disable-next-line @tanstack/query/exhaustive-deps
-    queryKey: ['useTokenPrice', tokenIndex],
-    queryFn: () => fetchTokenPrice(warpCore, tokenIndex),
-    enabled: !isNullish(tokenIndex),
+    queryKey: ['useTokenPrice', originToken?.coinGeckoId],
+    queryFn: () => fetchTokenPrice(originToken),
+    enabled: !!originToken?.coinGeckoId,
+    staleTime: TOKEN_PRICE_REFRESH_INTERVAL,
     refetchInterval: TOKEN_PRICE_REFRESH_INTERVAL,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
   });
 
   return { tokenPrice: data, isError, isLoading };
@@ -24,9 +27,7 @@ export function useTokenPrice({ tokenIndex }: TransferFormValues) {
 
 type CoinGeckoResponse = Record<string, { usd: number }>;
 
-async function fetchTokenPrice(warpCore: WarpCore, tokenIndex?: number): Promise<number | null> {
-  const originToken = getTokenByIndex(warpCore, tokenIndex);
-
+async function fetchTokenPrice(originToken?: Token): Promise<number | null> {
   if (!originToken || !originToken.coinGeckoId) return null;
   logger.debug('Fetching token price');
 

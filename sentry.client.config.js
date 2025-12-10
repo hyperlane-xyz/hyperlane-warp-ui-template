a9813778f7718af6decd49da7935ad2bd771673f
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-12-10 16:04:46 +0000

    style: run prettier

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,5 +1,5 @@
-import { sentryDefaultConfig } from './sentry.default.config';
 import * as Sentry from '@sentry/nextjs';
+import { sentryDefaultConfig } from './sentry.default.config';
 
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
   Sentry.init({
--- sentry.default.config.js
+++ sentry.default.config.js
@@ -1,23 +1,23 @@
 const filters = [
   // Hyperlane custom set
-  "trap returned falsish for property", // Error from cosmos wallet lib
-  "not established yet", // Same, bug with their WC integration ^
-  "Refused to create a WebAssembly object", // CSP blocking wasm
-  "call to WebAssembly.instantiate", // Same ^
-  "Request rejected", // Unknown noise during Next.js init
-  "WebSocket connection failed for host", // WalletConnect flakiness
-  "Socket stalled when trying to connect", // Same ^
-  "Request expired. Please try again.", // Same^
-  "Failed to publish payload", // Same ^
+  'trap returned falsish for property', // Error from cosmos wallet lib
+  'not established yet', // Same, bug with their WC integration ^
+  'Refused to create a WebAssembly object', // CSP blocking wasm
+  'call to WebAssembly.instantiate', // Same ^
+  'Request rejected', // Unknown noise during Next.js init
+  'WebSocket connection failed for host', // WalletConnect flakiness
+  'Socket stalled when trying to connect', // Same ^
+  'Request expired. Please try again.', // Same^
+  'Failed to publish payload', // Same ^
   // Some recommendations from https://docs.sentry.io/platforms/javascript/configuration/filtering
-  "top.GLOBALS",
-  "originalCreateNotification",
-  "canvas.contentDocument",
-  "MyApp_RemoveAllHighlights",
-  "atomicFindClose",
-  "Wallet is not initialized",
-  "region has been blocked from accessing this service"
-]
+  'top.GLOBALS',
+  'originalCreateNotification',
+  'canvas.contentDocument',
+  'MyApp_RemoveAllHighlights',
+  'atomicFindClose',
+  'Wallet is not initialized',
+  'region has been blocked from accessing this service',
+];
 
 export const sentryDefaultConfig = {
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
@@ -28,18 +28,14 @@ export const sentryDefaultConfig = {
   defaultIntegrations: false,
   integrations: [],
   beforeSend(event, hint) {
-    if (event && event.message && 
-      filters.find((f) => event.message.match(f))) 
-    {
+    if (event && event.message && filters.find((f) => event.message.match(f))) {
       return null;
     }
 
     const error = hint.originalException;
-    if (error && error.message && 
-      filters.find((f) => error.message.match(f))) 
-    {
+    if (error && error.message && filters.find((f) => error.message.match(f))) {
       return null;
-    } 
+    }
 
     delete event.user;
     return event;

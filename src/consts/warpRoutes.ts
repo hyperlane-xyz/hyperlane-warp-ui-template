ae208301a570095e128a122a1effc73044ef14bf
christopherbrumm <christopherbrumm2000@gmail.com>
2025-12-12 13:09:01 +0100

    chore: add old arbitrum route

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -35,6 +35,18 @@ export const warpRouteConfigs: WarpCoreConfig = {
           intermediateIbcDenom:
             'ibc/773B4D0A3CD667B2275D5A4A7A2F0909C0BA0F4059C0B9181E680DDF4965DCC7',
         },
+        // To Arbitrum via Neutron
+        {
+          token: 'ethereum|arbitrum|0xD56734d7f9979dD94FAE3d67C7e928234e71cD4C',
+          type: TokenConnectionType.IbcHyperlane,
+          sourcePort: 'transfer',
+          sourceChannel: 'channel-8',
+          intermediateChainName: 'neutron',
+          intermediateRouterAddress:
+            'neutron1jyyjd3x0jhgswgm6nnctxvzla8ypx50tew3ayxxwkrjfxhvje6kqzvzudq',
+          intermediateIbcDenom:
+            'ibc/773B4D0A3CD667B2275D5A4A7A2F0909C0BA0F4059C0B9181E680DDF4965DCC7',
+        },
         // To Stride
         {
           token:
@@ -42,30 +54,6 @@ export const warpRouteConfigs: WarpCoreConfig = {
           type: TokenConnectionType.Ibc,
           sourcePort: 'transfer',
           sourceChannel: 'channel-4',
-        },
-      ],
-    },
-
-    // TIA Celestia to Eclipse via Stride
-    {
-      chainName: 'celestia',
-      standard: TokenStandard.CosmosIbc,
-      name: 'TIA.s',
-      symbol: 'TIA.s',
-      decimals: 6,
-      addressOrDenom: 'utia',
-      logoURI: '/deployments/warp_routes/TIA/logo.svg',
-      connections: [
-        {
-          token: 'sealevel|eclipsemainnet|BpXHAiktwjx7fN6M9ST9wr6qKAsH27wZFhdHEhReJsR6',
-          type: TokenConnectionType.IbcHyperlane,
-          sourcePort: 'transfer',
-          sourceChannel: 'channel-4',
-          intermediateChainName: 'stride',
-          intermediateRouterAddress:
-            'stride1pvtesu3ve7qn7ctll2x495mrqf2ysp6fws68grvcu6f7n2ajghgsh2jdj6',
-          intermediateIbcDenom:
-            'ibc/BF3B4F53F3694B66E13C23107C84B6485BD2B96296BB7EC680EA77BBA75B4801',
         },
       ],
     },

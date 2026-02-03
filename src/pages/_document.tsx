8906aa63f5a112399406a9a03405aa4f9142847f
Xaroz <jasonguo2013jg@gmail.com>
2026-02-03 12:39:26 -0400

    chore: load page in bg color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,12 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import { APP_DESCRIPTION, APP_NAME, APP_URL, BRAND_COLOR, MAIN_FONT } from '../consts/app';
+import {
+  APP_DESCRIPTION,
+  APP_NAME,
+  APP_URL,
+  BACKGROUND_COLOR,
+  BRAND_COLOR,
+  MAIN_FONT,
+} from '../consts/app';
 
 export default function Document() {
   return (
@@ -30,7 +37,10 @@ export default function Document() {
         <meta property="og:image" content={`${APP_URL}/logo.png`} />
         <meta property="og:description" content={APP_DESCRIPTION} />
       </Head>
-      <body className={`${MAIN_FONT.variable} font-sans text-black`}>
+      <body
+          className={`${MAIN_FONT.variable} font-sans text-black`}
+          style={{ backgroundColor: BACKGROUND_COLOR }}
+        >
         <Main />
         <NextScript />
       </body>

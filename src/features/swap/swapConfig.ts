05ec6ca1b6fe4132b817368050bf00a3aed1e6ae
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-02-09 23:16:03 +0000

    chore(ui): add Base USDC address to swap config

--- src/features/swap/swapConfig.ts
+++ src/features/swap/swapConfig.ts
@@ -9,7 +9,7 @@ export const SWAP_CONTRACTS = {
   icaRouterArb: '0xCd2858B6bCaA9b628EBc4892F578b7d37E9ec229',
   icaRouterBase: '0x5ed29F0f32636CC69b0c189D5ec82C09DE7Cb0a7',
   usdcArb: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
-  usdcBase: '',
+  usdcBase: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
 };
 
 export const DEFAULT_SLIPPAGE = 0.005;

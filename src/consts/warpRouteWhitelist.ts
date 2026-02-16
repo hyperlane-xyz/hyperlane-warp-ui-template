ff14e0f46a89dfdf707cc00ace0a1a0d0c109aee
Jamin <57451149+yjamin@users.noreply.github.com>
2026-02-16 16:43:31 +0100

    chore: enable flowmmainet (#948)

--- src/consts/warpRouteWhitelist.ts
+++ src/consts/warpRouteWhitelist.ts
@@ -127,7 +127,7 @@ export const warpRouteWhitelist: Array<string> | null = [
 
   // cbBTC routes
   'cbBTC/base-ethereum-superseed',
-  // 'cbBTC/ethereum-flowmainnet',
+  'cbBTC/ethereum-flowmainnet',
 
   // ezETH routes
   'ezETH/renzo-prod',

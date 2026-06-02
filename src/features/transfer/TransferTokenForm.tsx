7c9f52d6d03ae39ca6f0d0dd0e8acd2be96580f1
Xaroz <jasonguo2013jg@gmail.com>
2026-06-02 13:52:05 -0400

    fix: merge conflicts

--- src/features/transfer/TransferTokenForm.tsx
+++ src/features/transfer/TransferTokenForm.tsx
@@ -60,11 +60,12 @@ import {
 import { ImportTokenButton } from '../tokens/ImportTokenButton';
 import { TokenSelectField } from '../tokens/TokenSelectField';
 import { useTokenPrices } from '../tokens/useTokenPrice';
-<<<<<<< HEAD
-import { checkTokenHasRoute, findConnectedDestinationToken, getTokenKey } from '../tokens/utils';
-=======
-import { checkTokenHasRoute, findConnectedDestinationToken, findRouteToken } from '../tokens/utils';
->>>>>>> origin/main
+import {
+  checkTokenHasRoute,
+  findConnectedDestinationToken,
+  findRouteToken,
+  getTokenKey,
+} from '../tokens/utils';
 import { WalletConnectionWarning } from '../wallet/WalletConnectionWarning';
 import { WalletDropdown } from '../wallet/WalletDropdown';
 import { getInterchainQuote, getTotalFee, getTransferToken } from './fees';

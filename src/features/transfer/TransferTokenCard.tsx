4d8ad5bdbedcc9c2e4a595ea37980c05d3e600be
Xaroz <jasonguo2013jg@gmail.com>
2026-06-16 16:10:26 -0400

    refactor: remove legacy transfer form UI
    - Delete unused bridge-only form and token picker components after unified form rollout
    - Keep current unified token picker components as the only active selection UI

--- src/features/transfer/TransferTokenCard.tsx
+++
@@ -1,9 +0,0 @@
-import { TransferTokenForm } from './TransferTokenForm';
-
-export function TransferTokenCard() {
-  return (
-    <div className="relative w-100 sm:w-[31rem]">
-      <TransferTokenForm />
-    </div>
-  );
-}

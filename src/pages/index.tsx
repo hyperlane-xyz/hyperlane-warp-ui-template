5eecb93aa5fe10fc7f3e665c9ee6b468e62d0556
Xaroz <jasonguo2013jg@gmail.com>
2026-06-16 16:40:25 -0400

    refactor: use index route for unified home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next';
 
 import { HomeView } from '../components/layout/HomeView';
 
-// Optional catch-all: serves /, /bridge, /swap from one unified page.
 const Home: NextPage = () => {
   return <HomeView />;
 };
